b762f784ee4b5a2ec20457e6e7d13e93b74211c0
Li Yanhui <whimet@gmail.com>
2018-09-16 21:59:13 +1000

    Build v1.2.0

--- dist/v1.2.0/js/util.js
+++ dist/v1.2.0/js/util.js
@@ -1,6 +1,6 @@
 function getUrlParam (param) {
-  var codedParam = (new RegExp(param + '=([^&]*)')).exec(window.location.search)[1];
-  return decodeURIComponent(codedParam);
+  var matchResult = (new RegExp(param + '=([^&]*)')).exec(window.location.search);
+  return matchResult && decodeURIComponent(matchResult[1]);
 };
 
 //https://gist.github.com/hagenburger/500716
