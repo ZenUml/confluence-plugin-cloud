2d5058989566b92a403f99e65101f538f585c0ef
PengXiao <eagle.xiao@gmail.com>
2019-12-01 09:24:58 +1100

    Added functions for installed/uninstalled.

--- functions/index.js
+++ functions/index.js
@@ -4,3 +4,11 @@ const admin = require('firebase-admin');
 exports.renderAttachment = functions.https.onRequest((request, response) => {
     response.send(`<ac:image> <ri:attachment ri:filename="zenuml-${request.query.uuid}.png" /> </ac:image>`);
 });
+
+exports.installedEndpoint = functions.https.onRequest((request, response) => {
+  response.status(200).send(`OK`);
+});
+
+exports.uninstalledEndpoint = functions.https.onRequest((request, response) => {
+  response.status(200).send(`OK`);
+});
