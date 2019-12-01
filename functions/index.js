ac3a672dedeef57c706ae69f971cd9de9556f1e8
PengXiao <eagle.xiao@gmail.com>
2019-12-01 15:09:50 +1100

    Adding log to the functions.

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,17 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
 exports.renderAttachment = functions.https.onRequest((request, response) => {
-    response.send(`<ac:image> <ri:attachment ri:filename="zenuml-${request.query.uuid}.png" /> </ac:image>`);
+  response.send(`<ac:image> <ri:attachment ri:filename="zenuml-${request.query.uuid}.png" /> </ac:image>`);
 });
 
 exports.installedEndpoint = functions.https.onRequest((request, response) => {
+  console.log('request.body.baseUrl:', request.body.baseUrl);
+  console.log('request.clientKey:', request.body.clientKey);
   response.status(200).send(`OK`);
 });
 
 exports.uninstalledEndpoint = functions.https.onRequest((request, response) => {
+  console.log('request.body.baseUrl:', request.body.baseUrl);
+  console.log('request.clientKey:', request.body.clientKey);
   response.status(200).send(`OK`);
 });
