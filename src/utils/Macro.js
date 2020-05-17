8e973601cdd991890fce033d550a1e01e36b3b2e
PengXiao <eagle.xiao@gmail.com>
2020-05-17 21:56:38 +1000

    Cache key and versionNumber when loading.
    You cannot call confluence.getXXX in onSubmit().

--- src/utils/Macro.js
+++ src/utils/Macro.js
@@ -13,6 +13,8 @@ BookService BookRepository Receipt Notification
 }
 `
   _confluence;
+  _key;
+  _versionNumber;
   constructor(confluence) {
     this._confluence = confluence;
   }
@@ -46,7 +48,9 @@ BookService BookRepository Receipt Notification
       if (!key) {
         resolve(null)
       } else {
+        this._key = key
         this._confluence.getContentProperty(this.propertyKey(key), (cp) => {
+          this._versionNumber = cp?.version?.number
           resolve(cp)
         })
       }
@@ -57,11 +61,12 @@ BookService BookRepository Receipt Notification
     return (await this.getContentProperty())?.value || await this.getMacroBody() || this.EXAMPLE
   }
 
+  // Warning! Do not call getXXX in onSubmit. Do retest if you want to call getXXX.
+  // It does not work as of 17th May 2020. That is why we have stored key and version
   async onSubmit(code) {
-    const contentProp = await this.getContentProperty();
-    const key = contentProp?.key || uuidv4()
+    const key = this._key || uuidv4()
     this._confluence.saveMacro({uuid: key, updatedAt: new Date()}, code)
-    const versionNumber = contentProp?.version?.number;
+    const versionNumber = this._versionNumber
     const contentProperty = {
       key: this.propertyKey(key),
       value: code,
