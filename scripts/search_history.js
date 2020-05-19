fa8c82e3e396fa182aa7402287b3e1ca0e064234
Yanhui Li <whimet@gmail.com>
2020-05-19 22:14:49 +1000

    search_history script writes restored macro body into content property

--- scripts/search_history.js
+++ scripts/search_history.js
@@ -6,7 +6,9 @@
   const zenumlLite = 'zenuml-sequence-macro-lite';
   const zenumlFull = 'zenuml-sequence-macro';
 
-  const macroBodySelector = `structured-macro[name=${zenumlLite}],structured-macro[name=${zenumlFull}] plain-text-body`;
+  const macroSelector = `structured-macro[name=${zenumlLite}],structured-macro[name=${zenumlFull}]`;
+  const macroParamSelector = `parameter[name=uuid]`;
+  const macroBodySelector = `plain-text-body`;
 
   const stripNamespacePrefix = (xml) => xml
     .replace(/<[^</> ]+:/g, '<') //e.g. <ac:link>
@@ -17,11 +19,19 @@
 
   const wrapXml = (xml) => `<root>${xml}</root>`;
 
-  const extract = (xml) => Array.from(parse(wrapXml(xml)).querySelectorAll(macroBodySelector)).map(e => e.textContent);
+  const parseMacros = (xml) => {
+    // console.log(xml);
+    return Array.from(parse(wrapXml(xml)).querySelectorAll(macroSelector)).map(m => {
+      const param = m.querySelector(macroParamSelector);
+      const body = m.querySelector(macroBodySelector);
+      return {uuid: param && param.textContent, body: body && body.textContent};
+    })
+  };
 
   const versionUrl = (pageId, version) => `/wiki/plugins/viewstorage/viewpagestorage.action?pageId=${pageId}&pageVersion=${version}`;
 
-  const getVersion = (pageId, version) => fetch(versionUrl(pageId, version)).then(r => r.text()).then(extract);
+  const getVersion = (pageId, version) => 
+    fetch(versionUrl(pageId, version)).then(r => r.text()).then(x => Object.assign({version}, {macros: parseMacros(x)}));
 
   const getVersions = (pageId) => fetch(`/wiki/rest/api/content/${pageId}/version`).then(r => r.json());
 
@@ -29,24 +39,52 @@
 
   const getStorageFormat = (pageId) => fetch(`/wiki/plugins/viewstorage/viewpagestorage.action?pageId=${pageId}`).then(r => r.text()).then(x => ({pageId: pageId, content: x}));
 
-  //find latest version which has non-empty macro body
+  //Find latest version which has at least one non-empty macro
+  //This isn't enough if a page has multiple macros, i.e. the second macro might have non-empty value in older version
   const findVersion = async (pageId, versions) => {
       for(let i = 0; i < versions.length; i++) {
           console.log(`checking page ${pageId} version ${versions[i].number}`);
-          const macros = await getVersion(pageId, versions[i].number);
-          if(macros.find(m => m.trim().length > 0)) {
-              return macros;
+          const version = await getVersion(pageId, versions[i].number);
+          if(version.macros.find(m => m.body && m.body.trim().length > 0)) {
+              return version;
           }
       }
   };
+
+  const sendData = async (url = '', data = {}, usePut = false) => {
+    const response = await fetch(url, {
+      method: usePut ? 'PUT' : 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  }
+
+  const getProperty = (pageId, key) => fetch(`/wiki/rest/api/content/${pageId}/property/${key}`).then(r => r.json());
+
+  const writeProperty = async (pageId, uuid, value) => {
+      const url = `/wiki/rest/api/content/${pageId}/property`;
+      const key = `zenuml-sequence-macro-${uuid}-body-restored`;
+      const property = await getProperty(pageId, key);
+      const existing = property.key === key;
+      await sendData(`${url}${existing ? `/${key}` : ''}`, {key: key, value: {code: value}, version: {number: existing ? property.version.number + 1 : 1}}, existing);
+      console.log(`${existing ? 'updated' : 'created'} content property ${key} in page ${pageId}:\n${value}`);
+    };
 
   const iteratePages = async () => {
     (await listPages()).forEach(async (pageId) => {
       const data = await getStorageFormat(pageId);
       if(data.content.includes(zenumlFull) || data.content.includes(zenumlLite)) {
         console.log(`checking page ${pageId}`);
-        await getVersions(pageId).then(r => findVersion(pageId, r.results))
-              .then(r => r && console.log(`found ${r.length} macro(s) in page ${pageId}:\n-----------------\n${r.join('\n-----------------\n')}`));
+        const versions = await getVersions(pageId);
+        const version = await findVersion(pageId, versions.results);
+        if(version) {
+          console.log(`found ${version.macros.length} macro(s) in page ${pageId} version ${version.version}:\n-----------------\n${version.macros.map(JSON.stringify).join('\n-----------------\n')}`);
+
+          version.macros.filter(m => m.body && m.body.trim().length > 0).forEach(async (m) => {
+            await writeProperty(pageId, m.uuid, m.body);
+          });
+        }
       }
     });
   };
