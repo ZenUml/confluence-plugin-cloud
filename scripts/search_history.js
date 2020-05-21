0b2f56cd26054f9ebf4eb480c280e4d9c64122df
Yanhui Li <whimet@gmail.com>
2020-05-21 22:16:28 +1000

    search_history now supports pagination

--- scripts/search_history.js
+++ scripts/search_history.js
@@ -35,23 +35,23 @@
 
   const getVersions = (pageId) => fetch(`/wiki/rest/api/content/${pageId}/version`).then(r => r.json());
 
-  const listPages = () => fetch('/wiki/rest/api/content').then(r => r.json()).then(d => d.results.filter(r => r.type === 'page').map(p => p.id));
-
   const getStorageFormat = (pageId) => fetch(`/wiki/plugins/viewstorage/viewpagestorage.action?pageId=${pageId}`).then(r => r.text()).then(x => ({pageId: pageId, content: x}));
 
   //Find latest version which has at least one non-empty macro
   //This isn't enough if a page has multiple macros, i.e. the second macro might have non-empty value in older version
-  const findVersion = async (pageId, versions) => {
+  const findVersion = async (pageId, versions, option) => {
       for(let i = 0; i < versions.length; i++) {
+        if(option.verbose) {
           console.log(`checking page ${pageId} version ${versions[i].number}`);
-          const version = await getVersion(pageId, versions[i].number);
-          if(version.macros.find(m => m.body && m.body.trim().length > 0)) {
-              return version;
-          }
+        }
+        const version = await getVersion(pageId, versions[i].number);
+        if(version.macros.find(m => m.body && m.body.trim().length > 0)) {
+            return version;
+        }
       }
   };
 
-  const sendData = async (url = '', data = {}, usePut = false) => {
+  const request = async (url = '', data = {}, usePut = false) => {
     const response = await fetch(url, {
       method: usePut ? 'PUT' : 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -62,33 +62,56 @@
 
   const getProperty = (pageId, key) => fetch(`/wiki/rest/api/content/${pageId}/property/${key}`).then(r => r.json());
 
-  const writeProperty = async (pageId, uuid, value) => {
-      const url = `/wiki/rest/api/content/${pageId}/property`;
-      const key = `zenuml-sequence-macro-${uuid}-body-restored`;
-      const property = await getProperty(pageId, key);
-      const existing = property.key === key;
-      await sendData(`${url}${existing ? `/${key}` : ''}`, {key: key, value: {code: value}, version: {number: existing ? property.version.number + 1 : 1}}, existing);
-      console.log(`${existing ? 'updated' : 'created'} content property ${key} in page ${pageId}:\n${value}`);
+  const writeProperty = async (page, uuid, value) => {
+    const pageId = page.id;
+    const title = page.title;
+    const url = `/wiki/rest/api/content/${pageId}/property`;
+    const key = `zenuml-sequence-macro-${uuid}-body-restored`;
+    const property = await getProperty(pageId, key);
+    const existing = property.key === key;
+    const data = {key: key, value: {code: value}, version: {number: existing ? property.version.number + 1 : 1}};
+
+    await request(`${url}${existing ? `/${key}` : ''}`, data, existing);
+    console.log(`${existing ? 'updated' : 'created'} content property ${key} in page "${title}":\n${value}`);
     };
 
-  const iteratePages = async () => {
-    (await listPages()).forEach(async (pageId) => {
+  const iteratePages = async (pages, option) => {
+    pages.forEach(async (page) => {
+      const pageId = page.id;
+      const title = page.title;
       const data = await getStorageFormat(pageId);
-      if(data.content.includes(zenumlFull) || data.content.includes(zenumlLite)) {
-        console.log(`checking page ${pageId}`);
-        const versions = await getVersions(pageId);
-        const version = await findVersion(pageId, versions.results);
-        if(version) {
-          console.log(`found ${version.macros.length} macro(s) in page ${pageId} version ${version.version}:\n-----------------\n${version.macros.map(JSON.stringify).join('\n-----------------\n')}`);
 
-          version.macros.filter(m => m.body && m.body.trim().length > 0).forEach(async (m) => {
-            await writeProperty(pageId, m.uuid, m.body);
-          });
+      if(data.content.includes(zenumlFull) || data.content.includes(zenumlLite)) {
+        if(option.verbose) {
+          console.log(`checking page ${title}`);
+        }
+        const versions = await getVersions(pageId);
+        const version = await findVersion(pageId, versions.results, option);
+        if(version) {
+          console.log(`found ${version.macros.length} macro(s) in page "${title}" version ${version.version}:\n--------------------------------------------\n${version.macros.map(JSON.stringify).join('\n--------------------------------------------\n')}`);
+
+          if(!option.dryrun) {
+            version.macros.filter(m => m.uuid && m.body && m.body.trim().length > 0).forEach(async (m) => {
+              await writeProperty(page, m.uuid, m.body);
+            });
+          }
         }
       }
     });
   };
 
-  await iteratePages();
+  async function search(url, option) {
+    if(option.verbose) {
+      console.log(`fetching ${url}`);
+    }
+    const data = await fetch(url).then(r => r.json());
+    await iteratePages(data.results, option);
+  
+    if(data._links.next) {
+      await search(`/wiki${data._links.next}`, option);
+    }
+  }
+
+  await search('/wiki/rest/api/content', {dryrun: true, verbose: false});
 
 })();
