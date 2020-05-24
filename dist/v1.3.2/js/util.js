69ef53b52e17acfa167529f935c9b34d6e3f2927
PengXiao <eagle.xiao@gmail.com>
2020-05-24 16:12:01 +1000

    Released the refactor AP code to Lite.

--- dist/v1.3.2/js/util.js
+++ dist/v1.3.2/js/util.js
@@ -1,21 +1,3 @@
-
-const EXAMPLE = `// Sample! Declare the participants (optional)
-BookService BookRepository Receipt Notification
-@Starter(User)
-"{id, dueDate, ...}" = BookService.Borrow(id) {
-  BookRepository.Update(id, onLoan)
-
-  // Send Event with "Source->Target:Event". "Source->" is optional
-  Notification:BOOK_ON_LOAN event with id, due date, etc. 
-  new Receipt(id, dueDate)
-}
-`
-
-function propertyKey(uuid) {
-  const macroKey = 'zenuml-sequence-macro';
-  return `${macroKey}-${uuid}-body`;
-}
-
 function getUrlParam (param) {
   var codedParam = (new RegExp(param + '=([^&]*)')).exec(window.location.search)[1];
   return decodeURIComponent(codedParam);
@@ -40,24 +22,6 @@ var JavaScript = {
 	  return script;
 	}
 };
-
-function loadMacroBody(callback) {
-  AP.require("request", function(request) {
-    let pageId = getUrlParam("pageId");
-    var pageVersion = getUrlParam("pageVersion");
-    var macroId = getUrlParam("macroId");
-    request({
-      url: "/rest/api/content/" + pageId +
-      "/history/" + pageVersion +
-      "/macro/id/" + macroId,
-      success: function(response) {
-	var macro = JSON.parse(response);
-	callback(macro.body);
-      }
-    });
-  });
-}
-
 function getConnectUrl() {
   return 'https://connect-cdn.atl-paas.net/all.js';
 }
