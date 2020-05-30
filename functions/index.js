91f7b5a29a960cad0ec611ca5f11f154d11c7b88
PengXiao <eagle.xiao@gmail.com>
2020-05-30 19:17:02 +1000

    Created function to handle dynamic app descriptor.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const descriptor = require('./atlassian-connect.json');
 
 exports.renderAttachment = functions.https.onRequest((request, response) => {
   response.send(`<ac:image> <ri:attachment ri:filename="zenuml-${request.query.uuid}.png" /> </ac:image>`);
@@ -17,3 +18,27 @@ exports.uninstalledEndpoint = functions.https.onRequest((request, response) => {
   console.log('request.body.pluginsVersion:', request.body.pluginsVersion);
   response.status(200).send(`OK`);
 });
+
+exports.descriptor = functions.https.onRequest((req, resp) => {
+  const url = req.url;
+  const basePath = url.substring(0, url.lastIndexOf('/'));
+  const self = url.substring(url.lastIndexOf('/'));
+  descriptor.baseUrl = `${req.protocol}://${req.hostname}${basePath}`;
+  descriptor.links.self = self;
+
+  const isLite = url.includes('-lite');
+  if (isLite) {
+    descriptor.key = 'com.zenuml.confluence-addon-lite';
+    descriptor.name = 'ZenUML Lite';
+    descriptor.description = 'ZenUML Lite add-on';
+    descriptor.enableLicensing = false;
+    descriptor.modules.dynamicContentMacros.forEach(macro => {
+      if (macro.key === 'zenuml-sequence-macro') {
+        macro.key = 'zenuml-sequence-macro-lite';
+        macro.name.value = 'ZenUML Lite';
+      }
+    })
+  }
+
+  resp.json(descriptor);
+})
