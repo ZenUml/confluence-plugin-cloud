60a9e6950efa6976d75f42a7ef19f4194c01bf2f
PengXiao <eagle.xiao@gmail.com>
2020-09-07 21:40:07 +1000

    Revert extracting isLite (it does not work for install/uninstall)

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,7 @@ exports.installedEndpoint = functions.https.onRequest((request, response) => {
     {
       DateTime: new Date().toLocaleString('en-AU'),
       ClientSite: request.body.baseUrl,
-      AppType: isLite(request) ? 'Lite' : 'Full',
+      AppType: 'Unknown',
       EventType: 'Install',
       Notes: ''
     }
@@ -27,7 +27,7 @@ exports.uninstalledEndpoint = functions.https.onRequest((request, response) => {
     {
       DateTime: new Date().toLocaleString('en-AU'),
       ClientSite: request.body.baseUrl,
-      AppType: isLite(request) ? 'Lite' : 'Full',
+      AppType: 'Unknown',
       EventType: 'Uninstall',
       Notes: ''
     }
@@ -35,19 +35,15 @@ exports.uninstalledEndpoint = functions.https.onRequest((request, response) => {
   response.status(200).send(`OK`);
 });
 
-function isLite(req) {
-  const url = req.url
-  const basePath = url.substring(0, url.lastIndexOf('/'))
-  const self = url.substring(url.lastIndexOf('/'))
-  descriptor.baseUrl = `${req.protocol}://${req.hostname}${basePath}`
-  // This is not necessary but works as a defense.
-  descriptor.links.self = self
-
-  return url.includes('lite')
-}
-
 exports.descriptor = functions.https.onRequest((req, resp) => {
-  const isLite = isLite(req)
+  const url = req.url;
+  const basePath = url.substring(0, url.lastIndexOf('/'));
+  const self = url.substring(url.lastIndexOf('/'));
+  descriptor.baseUrl = `${req.protocol}://${req.hostname}${basePath}`;
+  // This is not necessary but works as a defense.
+  descriptor.links.self = self;
+
+  const isLite = url.includes('lite');
   if (isLite) {
     descriptor.key = 'com.zenuml.confluence-addon-lite';
     descriptor.name = 'ZenUML Lite';
