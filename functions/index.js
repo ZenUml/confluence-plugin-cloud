c24c4bdc328417542f6ddff846635ea231e4e7e9
MrCoder <eagle.xiao@gmail.com>
2021-04-14 09:58:37 +1000

    Log uninstalled version.

--- functions/index.js
+++ functions/index.js
@@ -25,6 +25,7 @@ exports.installedEndpoint = functions.https.onRequest((request, response) => {
 });
 
 exports.uninstalledEndpoint = functions.https.onRequest((request, response) => {
+  console.log('version:', request.query.version);
   console.log('request.body.key:', request.body.key);
   console.log('request.body.baseUrl:', request.body.baseUrl);
   let key = request.body.key;
