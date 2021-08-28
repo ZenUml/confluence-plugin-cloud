ab92f3b1d5f4add94546b38b141a9e745fda17c2
MrCoder <eagle.xiao@gmail.com>
2021-08-28 17:28:39 +1000

    Converting ApWrapper to use typescript.

--- src/utils/ApWrapper.ts
+++ src/utils/ApWrapper.ts
@@ -3,15 +3,48 @@ import { getUrlParam } from './window';
 // Each iFrame provides context for only one macro.
 // getMacroData returns the macro data for the CURRENT macro.
 // ApWrapper converts callback to Promise and also encapsulates
+interface Confluence {
+  getMacroData: (arg0: (data: any) => void) => void;
+  getMacroBody: (arg0: (body: any) => void) => void;
+  getContentProperty: (arg0: any, arg1: (cp: any) => void) => void;
+  setContentProperty: (arg0: any, arg1: (result: any) => void) => void;
+  saveMacro: (arg0: any, arg1: any) => void;
+}
+
+interface ApRequestFunc {
+  (arg0: { url: string; type: string; contentType?: string; data?: string; }): any
+}
+
+interface AP {
+  confluence: Confluence;
+  request: ApRequestFunc;
+  navigator: any;
+  dialog: any;
+}
+
+interface MacroData {
+  uuid: string
+}
+
+interface ContentProperty {
+  value: { code: string }
+}
+
+interface ContentPropertyIn {
+}
+
+interface MacroParams {
+}
+
 // custom content APIs.
 export default class ApWrapper {
-  _confluence;
-  _request;
-  _navigator;
-  _dialog;
-  _macroIdentifier;
+  _confluence: Confluence;
+  _request: ApRequestFunc;
+  _navigator: any;
+  _dialog: any;
+  _macroIdentifier: string;
 
-  constructor(ap, macroIdentifier) {
+  constructor(ap: AP, macroIdentifier: string) {
     this._macroIdentifier = macroIdentifier;
     this._confluence = ap.confluence;
     this._request = ap.request;
@@ -19,11 +52,12 @@ export default class ApWrapper {
     this._dialog = ap.dialog;
   }
 
-  getMacroData() {
+  getMacroData(): Promise<MacroData|null> {
     return new Promise(((resolve) => {
       try {
         this._confluence.getMacroData((data) => {
           resolve(data)
+          console.debug('Loaded macro data:', data)
         })
       } catch (e) {
         // eslint-disable-next-line
@@ -34,7 +68,7 @@ export default class ApWrapper {
   }
 
   //FIXME: this method throws error in custom content viewer
-  getMacroBody() {
+  getMacroBody(): Promise<string|null> {
     return new Promise((resolve) => {
       try {
         this._confluence.getMacroBody((body) => {
@@ -48,12 +82,27 @@ export default class ApWrapper {
     })
   }
 
-  propertyKey(uuid) {
+  propertyKey(uuid: string) {
     const macroKey = `zenuml-${this._macroIdentifier}-macro`;
     return `${macroKey}-${uuid}-body`;
   }
 
-  getContentProperty(key) {
+  async getContentProperty2() {
+    let macroData = await this.getMacroData();
+    const uuid = macroData?.uuid;
+    if(!uuid) {
+      throw '`uuid` is empty. This diagram was not properly saved.'
+    }
+    let key = this.propertyKey(uuid);
+    let property = await this.getContentProperty(key);
+    if(!property) {
+      throw 'property is not find with key:' + key;
+    }
+    let {value} = property;
+    return {code: value.code};
+  }
+
+  getContentProperty(key: any): Promise<ContentProperty|null> {
     return new Promise(resolve => {
       try {
         this._confluence.getContentProperty(key, (cp) => {
@@ -67,7 +116,7 @@ export default class ApWrapper {
     })
   }
 
-  async setContentProperty(content) {
+  async setContentProperty(content: ContentPropertyIn) {
     return new Promise((resolve, reject) => {
       this._confluence.setContentProperty(content, (result) => {
         if(result.error) {
@@ -81,14 +130,14 @@ export default class ApWrapper {
     })
   }
 
-  saveMacro(params, body) {
+  saveMacro(params: MacroParams, body: string) {
     this._confluence.saveMacro(params, body)
   }
 
   getSpaceKey() { //TODO: cacheable
     const navigator = this._navigator;
     return new Promise((resolv) => {
-      navigator.getLocation((data) => {
+      navigator.getLocation((data: any) => {
         resolv(data.context.spaceKey);
       });
     });
@@ -97,7 +146,7 @@ export default class ApWrapper {
   getPageId() { //TODO: cacheable
     const navigator = this._navigator;
     return new Promise((resolve) => {
-      navigator.getLocation((data) => {
+      navigator.getLocation((data: { context: { contentId: unknown; }; }) => {
         resolve(data.context.contentId);
       });
     });
@@ -115,11 +164,11 @@ export default class ApWrapper {
     return `ac:${getUrlParam('addonKey')}:${this.getContentKey()}`;
   }
 
-  parseCustomContentResponse(response) {
+  parseCustomContentResponse(response: { body: string; }) {
     return response && response.body && JSON.parse(response.body);
   }
 
-  async createCustomContent(uuid, content) {
+  async createCustomContent(uuid: string, content: object) {
     const spaceKey = await this.getSpaceKey(); //TODO: cacheable
     const type = this.getCustomContentType();
     const bodyData = {
@@ -145,7 +194,7 @@ export default class ApWrapper {
     return this.parseCustomContentResponse(response);
   }
 
-  async updateCustomContent(contentObj, newBody) {
+  async updateCustomContent(contentObj: { type: any; title: any; version: { number: number; }; id: any; }, newBody: any) {
     const spaceKey = await this.getSpaceKey();
     const bodyData = {
       "type": contentObj.type,
@@ -173,7 +222,7 @@ export default class ApWrapper {
     return this.parseCustomContentResponse(response);
   }
 
-  async getCustomContentByTitle(type, title) {
+  async getCustomContentByTitle(type: any, title: any) {
     const spaceKey = await this.getSpaceKey();
     const url = `/rest/api/content?type=${type}&title=${title}&spaceKey=${spaceKey}&expand=children,history,version.number`;
     const results = JSON.parse((await this._request({type: 'GET', url})).body).results;
@@ -186,14 +235,14 @@ export default class ApWrapper {
     return null;
   }
 
-  async getCustomContentById(id) {
+  async getCustomContentById(id: string) {
     const url = `/rest/api/content/${id}?expand=body.raw,version.number`;
     const response = await this._request({type: 'GET', url});
     const customContent = this.parseCustomContentResponse(response);
     return Object.assign({}, customContent, {value: JSON.parse(customContent.body.raw.value)});
   }
 
-  async saveCustomContent(customContentId, uuid, value) {
+  async saveCustomContent(customContentId: string, uuid: string, value: object) {
     if(customContentId) {
       const existing = await this.getCustomContentById(customContentId);
       return await this.updateCustomContent(existing, value);
@@ -206,7 +255,7 @@ export default class ApWrapper {
     const dialog = this._dialog;
     return new Promise((resolv) => {
       try {
-        dialog.getCustomData(data => {
+        dialog.getCustomData((data: unknown) => {
           // eslint-disable-next-line
           console.log('custom data:', data);
           resolv(data);
