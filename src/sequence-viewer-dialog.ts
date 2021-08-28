51aacb86a4c75077995c49cf632e345ce87ee74e
MrCoder <eagle.xiao@gmail.com>
2021-08-28 14:41:13 +1000

    Extracted methods in sequence viewer dialog.

--- src/sequence-viewer-dialog.ts
+++ src/sequence-viewer-dialog.ts
@@ -3,37 +3,46 @@ import Vuex from 'vuex'
 
 import './assets/tailwind.css'
 // @ts-ignore
-import { Version, SeqDiagram, DiagramFrame, Store } from 'vue-sequence'
+import {DiagramFrame, Store} from 'vue-sequence'
 import 'vue-sequence/dist/vue-sequence.css'
 
-// @ts-ignore
-import Va from 'vue-atlas'
-import 'vue-atlas/dist/vue-atlas.css'
 import AP from './utils/AP'
 import SequenceDiagramLoader from './utils/SequenceDiagramLoader'
 import ConfluenceWrapper from "@/utils/ConfluenceWrapper";
 
 Vue.use(Vuex)
-Vue.use(Va)
 
 console.log('From sequence viewer dialog');
-const confluenceWrapper = new ConfluenceWrapper(AP)
-let sequenceDiagramLoader = new SequenceDiagramLoader(confluenceWrapper);
-let code = 'A.method1';
-// @ts-ignore
-window.sequenceDiagramLoader = sequenceDiagramLoader
-async function main() {
+
+async function getCode() {
+  const confluenceWrapper = new ConfluenceWrapper(AP)
+  let sequenceDiagramLoader = new SequenceDiagramLoader(confluenceWrapper);
+  // @ts-ignore
+  window.sequenceDiagramLoader = sequenceDiagramLoader
   let content = await sequenceDiagramLoader.load();
-  code = content.code || 'B.method2';
+  return content.code || 'A.method';
+}
+
+function configStore(code: string) {
   let storeInstance = Store();
   const store = new Vuex.Store(storeInstance);
   store.commit('code', code);
-  if(document.getElementById('app')) {
+  return store;
+}
+
+function mountDiagramFrame(store: any) {
+  if (document.getElementById('app')) {
     new Vue({
       store,
       render: h => h(DiagramFrame) // with this method, we don't need to use full version of vew
     }).$mount('#app')
   }
+}
+
+async function main() {
+  let code = await getCode();
+  const store = configStore(code);
+  mountDiagramFrame(store);
 }
 
 main();
