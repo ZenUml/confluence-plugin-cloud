89579b59e22b27ba7c9ee898b0d67413738aef80
MrCoder <eagle.xiao@gmail.com>
2021-08-28 14:58:02 +1000

    Extracted sequence-view-store.ts

--- src/sequence-viewer-dialog.ts
+++ src/sequence-viewer-dialog.ts
@@ -1,48 +1,24 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-
 import './assets/tailwind.css'
 // @ts-ignore
-import {DiagramFrame, Store} from 'vue-sequence'
+import {DiagramFrame} from 'vue-sequence'
 import 'vue-sequence/dist/vue-sequence.css'
 
-import AP from './utils/AP'
-import SequenceDiagramLoader from './utils/SequenceDiagramLoader'
-import ConfluenceWrapper from "@/utils/ConfluenceWrapper";
-
-Vue.use(Vuex)
-
+import configStoreAsyncFn from "./utils/sequence-view-store";
 console.log('From sequence viewer dialog');
 
-async function getCode() {
-  const confluenceWrapper = new ConfluenceWrapper(AP)
-  let sequenceDiagramLoader = new SequenceDiagramLoader(confluenceWrapper);
-  // @ts-ignore
-  window.sequenceDiagramLoader = sequenceDiagramLoader
-  let content = await sequenceDiagramLoader.load();
-  return content.code || 'A.method';
-}
-
-function configStore(code: string) {
-  let storeInstance = Store();
-  const store = new Vuex.Store(storeInstance);
-  store.commit('code', code);
-  return store;
-}
-
-function mountDiagramFrame(store: any) {
-  if (document.getElementById('app')) {
+function mountDiagramFrame(store: any, id: string) {
+  if (document.getElementById(id)) {
     new Vue({
       store,
       render: h => h(DiagramFrame) // with this method, we don't need to use full version of vew
-    }).$mount('#app')
+    }).$mount(`#${id}`)
   }
 }
 
 async function main() {
-  let code = await getCode();
-  const store = configStore(code);
-  mountDiagramFrame(store);
+  mountDiagramFrame(await configStoreAsyncFn(), 'app');
 }
 
-main();
+// We do not have to export main(), but otherwise IDE shows a warning
+export default main();
---
+++ src/utils/sequence-view-store.ts
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// @ts-ignore
+import {Store} from 'vue-sequence'
+
+import ConfluenceWrapper from "@/utils/ConfluenceWrapper";
+import AP from "@/utils/AP";
+import SequenceDiagramLoader from "@/utils/SequenceDiagramLoader";
+Vue.use(Vuex)
+
+async function getCode() {
+  const confluenceWrapper = new ConfluenceWrapper(AP)
+  let sequenceDiagramLoader = new SequenceDiagramLoader(confluenceWrapper);
+  // @ts-ignore
+  window.sequenceDiagramLoader = sequenceDiagramLoader
+  let content = await sequenceDiagramLoader.load();
+  return content.code || 'A.method';
+}
+
+async function configStoreAsyncFn() {
+  let storeInstance = Store();
+  const store = new Vuex.Store(storeInstance);
+  store.commit('code', await getCode());
+  return store;
+}
+
+export default configStoreAsyncFn
