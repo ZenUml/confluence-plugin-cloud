3b1cab9680204ca237c2672f59a4bda0360bb4f8
Yanhui Li <whimet@gmail.com>
2021-10-24 18:44:22 +1100

    Test saving content property in dialog editor

--- tests/unit/BaseMacro2.spec.ts
+++ tests/unit/BaseMacro2.spec.ts
@@ -16,16 +16,16 @@ jest.mock('../../src/utils/uuid', () => {
 describe('BaseMacro2', () => {
   const contentId = 'content_id_1234';
 
-  beforeEach(() => {
-    helper.setUpUrlParam('contentKey=sequence');
+  function setUp(param: string) {
+    helper.setUpUrlParam(param);
 
     mockAp = new MockAp(contentId);
     mockApConfluence = mockAp.confluence;
     macro = new BaseMacro2(new ApWrapper2(mockAp));
-  });
-
+  };
 
   it('creates custom content if _customContentId is null', async () => {
+    setUp('contentKey=sequence');
     await macro.load();
 
     const customContentId = await macro.save({
@@ -37,6 +37,7 @@ describe('BaseMacro2', () => {
   })
 
   it('update custom content if _customContentId is not null', async () => {
+    setUp('contentKey=sequence');
     await macro.load();
 
     const customContentId = await macro.save({
@@ -45,5 +46,26 @@ describe('BaseMacro2', () => {
       source: DataSource.CustomContent
     });
     expect(customContentId).toBe(1234);
+  })
+
+  it('save content property in dialog editor', async () => {
+    setUp('contentKey=sequence&rendered.for=dialog-editor');
+
+    const key = 'zenuml-sequence-macro-abc-123-body';
+    const value = {code: 'a.foo'};
+
+    mockApConfluence.saveMacro({uuid: 'abc-123'}, JSON.stringify(value));
+    mockApConfluence.setContentProperty({key: key, version: {number: 1}, value});
+
+    const payload = await macro.load();
+    expect(payload.source).toBe(DataSource.ContentProperty);
+
+    const diagram = {
+      diagramType: DiagramType.Sequence,
+      code: 'A.m',
+      source: DataSource.ContentProperty
+    };
+    await macro.save(diagram);
+    mockApConfluence.getContentProperty(key, (content) => expect(content).toEqual({version: {number: 2}, key, value: diagram}));
   })
 })
