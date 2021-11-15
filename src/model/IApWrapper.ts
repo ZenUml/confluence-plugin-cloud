43dd67d84a70eedbb00409f56cbb7ddacba390fe
MrCoder <eagle.xiao@gmail.com>
2021-11-15 22:34:52 +1100

    Re-organized methods in IApWrapper.ts.

--- src/model/IApWrapper.ts
+++ src/model/IApWrapper.ts
@@ -14,26 +14,21 @@ export interface IApWrapper {
   versionType: VersionType;
 
   isLite(): boolean;
-
-  getMacroData(): Promise<IMacroData | undefined>;
-
-  getMacroBody(): Promise<string | undefined>;
-
-  getContentProperty2(): Promise<IContentPropertyNormalised | undefined>;
-
-  getCustomContent(): Promise<ICustomContent | undefined>;
-
-  getCustomContentById(id: string): Promise<ICustomContent | undefined>;
-
   getPageId(): Promise<string>;
 
-  hasCustomContent(): boolean;
-
-  saveCustomContent(customContentId: string, value: Diagram): Promise<any>;
-
+  // Macro APIs
+  getMacroData(): Promise<IMacroData | undefined>;
+  getMacroBody(): Promise<string | undefined>;
   saveMacro(params: IMacroData, body: string): void;
 
-  createCustomContent(content: Diagram): Promise<any>;
+  // Content Property APIs
+  getContentProperty2(): Promise<IContentPropertyNormalised | undefined>;
 
+  // Custom Content APIs
+  hasCustomContent(): boolean;
+  getCustomContent(): Promise<ICustomContent | undefined>;
+  getCustomContentById(id: string): Promise<ICustomContent | undefined>;
+  createCustomContent(content: Diagram): Promise<any>;
   updateCustomContent(contentObj: ICustomContent, newBody: Diagram): Promise<any>;
+  saveCustomContent(customContentId: string, value: Diagram): Promise<any>;
 }
