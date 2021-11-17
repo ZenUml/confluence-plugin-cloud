c1a3a96665e6fbc9198edad3b30f140b63d79e1a
MrCoder <eagle.xiao@gmail.com>
2021-11-17 12:00:29 +1100

    It seems sufficient to mock window.location with new URL.

--- tests/unit/ApWrapper.spec.ts
+++ tests/unit/ApWrapper.spec.ts
@@ -2,32 +2,22 @@ import MockAp from "@/model/MockAp";
 import ApWrapper2 from "@/model/ApWrapper2";
 
 describe('ApWrapper', () => {
-  beforeEach(() => {
-    // See the following pattern at https://icing.space/2021/mocking-window-location-in-jest/
-    delete window.location;
-    // It seems sufficient to mock window.location with new URL
-    // @ts-ignore
-    window.location = new URL("https://zenuml.com/?contentKey=zenuml-content-sequence");
-    // window.location = Object.assign(new URL("https://zenuml.com/?contentKey=zenuml-content-sequence"), {
-    //   ancestorOrigins: "",
-    //   assign: jest.fn(),
-    //   reload: jest.fn(),
-    //   replace: jest.fn()
-    // });
-  });
 
   it('tells whether it is a lite version or full version', () => {
+    delete window.location;
+    // @ts-ignore
+    window.location = new URL("https://zenuml.com/?contentKey=zenuml-content-sequence");
     let mockAp = new MockAp();
     let apWrapper2 = new ApWrapper2(mockAp);
     expect(apWrapper2.isLite()).toBeFalsy();
   })
 
   it('tells whether it is a lite version or full version', () => {
-    let mockAp = new MockAp();
-    let apWrapper2 = new ApWrapper2(mockAp);
     delete window.location;
     // @ts-ignore
-    window.location = new URL('https://zenuml.com/?addonKey=com.zenuml.confluence-addon-lite')
+    window.location = new URL("https://zenuml.com/?contentKey=zenuml-content-sequence&addonKey=com.zenuml.confluence-addon-lite");
+    let mockAp = new MockAp();
+    let apWrapper2 = new ApWrapper2(mockAp);
     expect(apWrapper2.isLite()).toBeTruthy();
   })
 })
