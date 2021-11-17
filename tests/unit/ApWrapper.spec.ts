e3b9a6b8bd05b43f8d5b6ff0a2778e2295230894
MrCoder <eagle.xiao@gmail.com>
2021-11-17 23:04:03 +1100

    Refactored ApWrapper.spec.ts.

--- tests/unit/ApWrapper.spec.ts
+++ tests/unit/ApWrapper.spec.ts
@@ -1,30 +1,67 @@
 import MockAp from "@/model/MockAp";
 import ApWrapper2 from "@/model/ApWrapper2";
 
+function buildResponse(containerId: string, code: string) {
+  return {
+    body: JSON.stringify({
+      body: {
+        raw: {
+          value: JSON.stringify({
+            "code": code
+          })
+        }
+      },
+      container: {
+        id: containerId
+      }
+    })
+  };
+}
+
+function buildEnrichedCustomContent(containerId: string = "page-002", code: string = "A.method", isCopy: boolean = false) {
+  return {
+    body: {
+      raw: {
+        value: JSON.stringify({
+          "code": code
+        })
+      }
+    },
+    container: {
+      id: containerId
+    },
+    value: {
+      "code": code,
+      "isCopy": isCopy,
+      "source": "custom-content"
+    }
+  };
+}
+
+function setUpWindowLocation(query: string = "?contentKey=zenuml-content-sequence") {
+  delete window.location;
+  // @ts-ignore
+  window.location = new URL("https://zenuml.com/" + query);
+}
+
 describe('ApWrapper', () => {
 
   it('tells whether it is a lite version or full version', () => {
-    delete window.location;
-    // @ts-ignore
-    window.location = new URL("https://zenuml.com/?contentKey=zenuml-content-sequence");
+    setUpWindowLocation("?contentKey=zenuml-content-sequence");
     let mockAp = new MockAp();
     let apWrapper2 = new ApWrapper2(mockAp);
     expect(apWrapper2.isLite()).toBeFalsy();
   })
 
   it('tells whether it is a lite version or full version', () => {
-    delete window.location;
-    // @ts-ignore
-    window.location = new URL("https://zenuml.com/?contentKey=zenuml-content-sequence&addonKey=com.zenuml.confluence-addon-lite");
+    setUpWindowLocation("?contentKey=zenuml-content-sequence&addonKey=com.zenuml.confluence-addon-lite");
     let mockAp = new MockAp();
     let apWrapper2 = new ApWrapper2(mockAp);
     expect(apWrapper2.isLite()).toBeTruthy();
   })
 
   it('tells us the page id', async () => {
-    delete window.location;
-    // @ts-ignore
-    window.location = new URL("https://zenuml.com/?contentKey=zenuml-content-sequence");
+    setUpWindowLocation("?contentKey=zenuml-content-sequence");
     let mockAp = new MockAp();
     let apWrapper2 = new ApWrapper2(mockAp);
     const getLocationContext = jest.fn().mockImplementation(async () => {
@@ -36,50 +73,32 @@ describe('ApWrapper', () => {
     expect(await apWrapper2.getPageId()).toBe('page-001');
   })
 
-  it('gets custom content by id', async () => {
-    delete window.location;
-    // @ts-ignore
-    window.location = new URL("https://zenuml.com/?contentKey=zenuml-content-sequence");
+  it('gets custom content by id (on the original page)', async () => {
+    setUpWindowLocation("?contentKey=zenuml-content-sequence");
     let mockAp = new MockAp();
     let apWrapper2 = new ApWrapper2(mockAp);
     const _requestFn = jest.fn().mockImplementation(async () => {
-      return {
-      body: JSON.stringify({
-        body: {
-          raw: {
-            value: JSON.stringify({
-              "code": "A.method"
-            })
-          }
-        },
-        container: {
-          id: "page-002"
-        }
-      })
-    }});
+      return buildResponse("page-001", "A.method")});
     apWrapper2._requestFn = _requestFn.bind(apWrapper2);
 
-    const getPageId = jest.fn().mockImplementation(async () => {
-      return "page-002"
-    });
+    const getPageId = jest.fn().mockImplementation(async () => { return "page-001" });
     apWrapper2.getPageId = getPageId.bind(apWrapper2);
-    expect(await apWrapper2.getCustomContentById("custom-content-001")).toEqual({
-      body: {
-        raw: {
-          value: JSON.stringify({
-            "code": "A.method"
-          })
-        }
-      },
-      container: {
-        id: "page-002"
-      },
-      value: {
-        "code": "A.method",
-        "isCopy": false,
-        "source": "custom-content"
-      }
-    });
+    expect(await apWrapper2.getCustomContentById("custom-content-001"))
+      .toEqual(buildEnrichedCustomContent("page-001", "A.method", false));
+  })
+
+  it('gets custom content by id (on a different page)', async () => {
+    setUpWindowLocation("?contentKey=zenuml-content-sequence");
+    let mockAp = new MockAp();
+    let apWrapper2 = new ApWrapper2(mockAp);
+    const _requestFn = jest.fn().mockImplementation(async () => {
+      return buildResponse("page-002", "A.method")});
+    apWrapper2._requestFn = _requestFn.bind(apWrapper2);
+
+    const getPageId = jest.fn().mockImplementation(async () => { return "page-001" });
+    apWrapper2.getPageId = getPageId.bind(apWrapper2);
+    expect(await apWrapper2.getCustomContentById("custom-content-001"))
+      .toEqual(buildEnrichedCustomContent("page-002", "A.method", true));
   })
 
 })
