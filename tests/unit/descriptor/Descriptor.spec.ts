2f2bf8ca13fb1e6c43b1f9a6136bc51fceac831d
MrCoder <eagle.xiao@gmail.com>
2021-12-19 13:17:43 +1100

    Passed test for Full.

--- tests/unit/descriptor/Descriptor.spec.ts
+++ tests/unit/descriptor/Descriptor.spec.ts
@@ -1,11 +1,17 @@
 import Base from './atlassian-connect.base.json'
 import Full from './atlassian-connect.full.json';
 
+interface IDescriptor {
+  modules: {
+    dynamicContentMacros: Array<object>
+  }
+}
+
 class DescriptorBuilder {
-  private _base?: object;
+  private _base?: IDescriptor;
   private _version?: string;
 
-  from(base: object) {
+  from(base: IDescriptor) {
     this._base = base;
     return this;
   }
@@ -15,9 +21,44 @@ class DescriptorBuilder {
     return this;
   }
 
+  getCustomContentKeyForModule = (module: any, modules: any) => {
+    const macroType = module.key.includes('sequence') ? 'sequence' : 'graph';
+    const result = modules.customContent.filter((c:any) => c.key.includes(macroType));
+    if(result.length === 0) {
+      console.log(`Custom content not found for module ${module.key} in ${modules.customContent.map((c: any) => c.key)}`);
+    } else {
+      return result[0].key;
+    }
+  }
   full() {
+    this._base?.modules?.dynamicContentMacros.forEach(macro => {
+      const contentKey = this.getCustomContentKeyForModule(macro, this._base?.modules);
+      // @ts-ignore
+      macro.url = macro.url.replace('__CONTENT_KEY__', contentKey);
+      // @ts-ignore
+      if (macro.editor) {
+        // @ts-ignore
+        macro.editor.url = macro.editor?.url.replace('__CONTENT_KEY__', contentKey);
+      }
+
+      // @ts-ignore
+      if(macro.renderModes && macro.renderModes.default && macro.renderModes.default.url) {
+        // @ts-ignore
+        macro.renderModes.default.url = macro.renderModes.default.url.replace('__CONTENT_KEY__', contentKey);
+      }
+    });
+
+    // @ts-ignore
+    this._base?.modules?.generalPages?.forEach(page => {
+      const contentKey = this.getCustomContentKeyForModule(page, this._base?.modules);
+
+      page.url = page.url.replace('__CONTENT_KEY__', contentKey);
+    })
+
     let stringBase = JSON.stringify(this._base);
-    let stringFull = stringBase.replace(/__VERSION__/g, this._version || '');
+    let stringFull = stringBase
+      .replace(/__VERSION__/g, this._version || '')
+      .replace(/__ADDON_KEY__/g, 'com.zenuml.confluence-addon')
     return JSON.parse(stringFull);
   }
 }
@@ -25,9 +66,10 @@ class DescriptorBuilder {
 describe("Descriptor", () => {
   it("should generate a full descriptor", () => {
     let descriptorBuilder = new DescriptorBuilder();
-    descriptorBuilder.from(Base)
+    let base: IDescriptor = Base;
+    descriptorBuilder.from(base)
       .forVersion('2021.11');
     let full = descriptorBuilder.full();
-    expect(full).toBe(Full);
+    expect(full).toStrictEqual(Full);
   })
 })
