733458f24d28c592f7a7d2304ad03edcd8d0481a
MrCoder <eagle.xiao@gmail.com>
2021-12-19 13:21:27 +1100

    Added type information to descriptor.

--- tests/unit/descriptor/Descriptor.spec.ts
+++ tests/unit/descriptor/Descriptor.spec.ts
@@ -1,9 +1,26 @@
 import Base from './atlassian-connect.base.json'
 import Full from './atlassian-connect.full.json';
 
+interface IDynamicContentMacro {
+  url: string
+  editor?: {
+    url: string
+  }
+  renderModes?: {
+    default: {
+      url: string
+    }
+  }
+}
+
+interface IGeneralPage {
+  url: string
+}
+
 interface IDescriptor {
   modules: {
-    dynamicContentMacros: Array<object>
+    dynamicContentMacros: Array<IDynamicContentMacro>
+    generalPages: Array<IGeneralPage>
   }
 }
 
@@ -33,25 +50,18 @@ class DescriptorBuilder {
   full() {
     this._base?.modules?.dynamicContentMacros.forEach(macro => {
       const contentKey = this.getCustomContentKeyForModule(macro, this._base?.modules);
-      // @ts-ignore
       macro.url = macro.url.replace('__CONTENT_KEY__', contentKey);
-      // @ts-ignore
       if (macro.editor) {
-        // @ts-ignore
         macro.editor.url = macro.editor?.url.replace('__CONTENT_KEY__', contentKey);
       }
 
-      // @ts-ignore
       if(macro.renderModes && macro.renderModes.default && macro.renderModes.default.url) {
-        // @ts-ignore
         macro.renderModes.default.url = macro.renderModes.default.url.replace('__CONTENT_KEY__', contentKey);
       }
     });
 
-    // @ts-ignore
     this._base?.modules?.generalPages?.forEach(page => {
       const contentKey = this.getCustomContentKeyForModule(page, this._base?.modules);
-
       page.url = page.url.replace('__CONTENT_KEY__', contentKey);
     })
 
