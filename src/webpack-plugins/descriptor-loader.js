a1537162939b1c1a521bbf267b0fd30f66d4be53
MrCoder <eagle.xiao@gmail.com>
2021-12-19 21:48:30 +1100

    Generate both full and lite descriptor in dist folder.

--- src/webpack-plugins/descriptor-loader.js
+++ src/webpack-plugins/descriptor-loader.js
@@ -1,7 +1,9 @@
 const {DescriptorBuilder} = require("../descriptor/DescriptorBuilder");
 
 module.exports = function(source) {
-  const full = new DescriptorBuilder(JSON.parse(source)).lite();
+  const full = new DescriptorBuilder(JSON.parse(source)).full();
   this.emitFile(this.resourcePath.split('/').pop().split('.')[0] + '.json', JSON.stringify(full));
+  const lite = new DescriptorBuilder(JSON.parse(source)).lite();
+  this.emitFile(this.resourcePath.split('/').pop().split('.')[0] + '-lite.json', JSON.stringify(lite));
   return '';
 }
