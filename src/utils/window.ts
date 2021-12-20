b749863c3a9ddf3a5fbdf3848019c92a87866c27
MrCoder <eagle.xiao@gmail.com>
2021-12-21 09:57:23 +1100

    Track error in loading sequence diagrams.

--- src/utils/window.ts
+++ src/utils/window.ts
@@ -10,6 +10,10 @@ export function getUrlParam (param: string): string | undefined {
 }
 
 export function trackEvent(label: DiagramType | string, action: string, category: string) {
+  try {
     // @ts-ignore
-  window.gtag && window.gtag('event', action, {'event_category': category, 'event_label' : label});
+    window.gtag && window.gtag('event', action, {'event_category': category, 'event_label' : label});
+  } catch (e) {
+    console.error('Error in trackingEvent. Please report to our helpdesk:https://zenuml.atlassian.net/servicedesk/customer/portals')
+  }
 }
