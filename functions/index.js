f1a26f581fd56ad5b46bf9366f2dc2e461303833
Yanhui Li <whimet@gmail.com>
2022-01-05 22:15:59 +1100

    Show onboarding popup on new version

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@ const descriptor = require('./atlassian-connect.json');
 const SteinStore = require('stein-js-client');
 const store = new SteinStore('https://api.steinhq.com/v1/storages/5ed5fe9883c30d0425e2c433');
 
+const VERSION = '2022.01';
+
 exports.renderAttachment = functions.https.onRequest((request, response) => {
   response.send(`<ac:image> <ri:attachment ri:filename="zenuml-${request.query.uuid}.png" /> </ac:image>`);
 });
@@ -53,8 +55,6 @@ exports.descriptor = functions.https.onRequest((req, resp) => {
   // This is not necessary but works as a defense.
   data.links.self = self;
 
-  const VERSION = '2021.11';
-
   const replaceUrls = (modules, replaceFunction) => {
     modules.dynamicContentMacros.forEach(macro => {
       macro.url = replaceFunction(macro.url, macro);
@@ -69,6 +69,11 @@ exports.descriptor = functions.https.onRequest((req, resp) => {
     if(modules.generalPages) {
       modules.generalPages.forEach(page => {
         page.url = replaceFunction(page.url, page);
+      });
+    }
+    if(modules.webPanels) {
+      modules.webPanels.forEach(m => {
+        m.url = replaceFunction(m.url, m);
       });
     }
   }
@@ -111,8 +116,16 @@ exports.descriptor = functions.https.onRequest((req, resp) => {
 
   replaceUrls(data.modules, (url, module) => {
     let result = url.replace('__ADDON_KEY__', data.key);
-    const contentKey = getCustomContentKeyForModule(module, data.modules);
-    return result.replace('__CONTENT_KEY__', contentKey).replace('__VERSION__', VERSION);
+
+    if(result.includes('__CONTENT_KEY__')) {
+      const contentKey = getCustomContentKeyForModule(module, data.modules);
+      result = result.replace('__CONTENT_KEY__', contentKey);
+    }
+    if(result.includes('__POST_INSTALL_KEY__') && data.modules.postInstallPage) {
+      result = result.replace('__POST_INSTALL_KEY__', data.modules.postInstallPage.key);
+    }
+
+    return result.replace('__VERSION__', VERSION);
   });
 
   resp.json(data);
--- public/js/util.js
+++ public/js/util.js
@@ -1,3 +1,5 @@
+const HELP_DESK_URL = 'https://zenuml.atlassian.net/servicedesk/customer/portals';
+
 function getUrlParam (param) {
   let codeParams = (new RegExp(param + '=([^&]*)')).exec(window.location.search);
   if(codeParams && codeParams.length >= 1) {
@@ -39,4 +41,8 @@ function uuidv4() {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
   );
+}
+
+function reportError(error, context) {
+  console.error(`Error in ${context}. Please report to our helpdesk: ${HELP_DESK_URL}`, error);
 }
