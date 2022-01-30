c993a7e4266fc9571ae192f61daee835ba4eec42
Yanhui Li <whimet@gmail.com>
2022-01-30 23:09:19 +1100

    Update custom dimensions index

--- public/js/google-analytics.js
+++ public/js/google-analytics.js
@@ -1,7 +1,8 @@
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 
-const customDimensions = {dimension1: 'client_domain', dimension2: 'user_account_id', dimension3: 'confluence_space'};
+//custom dimensions are defined in Google Analytics console
+const customDimensions = {dimension14: 'client_domain', dimension15: 'user_account_id', dimension16: 'confluence_space'};
 
 gtag('js', new Date());
 gtag('config', 'UA-114996686-1', {cookie_flags: 'max-age=7200;secure;samesite=none', custom_map: customDimensions});
