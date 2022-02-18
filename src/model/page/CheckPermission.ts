f3568d088f9fdb6f512822ce65ad8b35370036f6
Peng Xiao <eagle.xiao@gmail.com>
2022-02-18 11:02:08 +1100

    Silent errors from CheckPermission.ts (#341)

--- src/model/page/CheckPermission.ts
+++ src/model/page/CheckPermission.ts
@@ -6,11 +6,17 @@ export interface PermissionCheckRequestFunc {
 }
 
 export default async function CheckPermission(pageId: string, userId: string, requestFn: PermissionCheckRequestFunc): Promise<boolean> {
-  const resp = await requestFn({
-    type: 'POST',
-    url: `/rest/api/content/${pageId}/permission/check`,
-    contentType: 'application/json',
-    data: JSON.stringify({subject: {type: 'user', identifier: userId}, operation: 'update'})
-  })
-  return JSON.parse(resp.body).hasPermission;
+  try {
+    const resp = await requestFn({
+      type: 'POST',
+      url: `/rest/api/content/${pageId}/permission/check`,
+      contentType: 'application/json',
+      data: JSON.stringify({subject: {type: 'user', identifier: userId}, operation: 'update'})
+    })
+    return JSON.parse(resp.body).hasPermission;
+  } catch (err) {
+    console.log('Current user may not have permission to check permission.')
+    console.info(err);
+    return false;
+  }
 }
