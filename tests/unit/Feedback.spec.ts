637fdd1451204687926094428f5ab36a0676ae0e
MrCoder <eagle.xiao@gmail.com>
2022-02-25 22:46:46 +1100

    Fixed test for feedback.

--- tests/unit/Feedback.spec.ts
+++ tests/unit/Feedback.spec.ts
@@ -3,7 +3,7 @@ import GetSupport from '@/components/SendFeedback.vue'
 
 describe('SendFeedback.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'Send feedback'
+    const msg = 'Feedback on the new layout engine'
     const wrapper = shallowMount(GetSupport, {
       propsData: { msg }
     })
