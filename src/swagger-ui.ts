940d57ec204dde4649bbf0447baecb5dd72a8f06
Yanhui Li <whimet@gmail.com>
2022-07-10 11:30:35 +1000

    viewer can load correctly

--- src/swagger-ui.ts
+++ src/swagger-ui.ts
@@ -1,5 +1,55 @@
 import SwaggerUIBundle from 'swagger-ui'
+import SpecListener from './utils/spec-listener'
+import BaseMacro2 from "./model/BaseMacro2";
+import ApWrapper2 from "@/model/ApWrapper2";
+import AP from "@/model/AP";
 
 // eslint-disable-next-line
 // @ts-ignore
 window.SwaggerUIBundle = SwaggerUIBundle;
+
+async function initializeMacro() {
+  const apWrapper = new ApWrapper2(AP);
+  await apWrapper.initializeContext();
+  
+  const macro = new BaseMacro2(apWrapper);
+  // await macro.load();
+
+  // @ts-ignore
+  window.macro = macro;
+  const {code} = await macro.load();
+  console.log('-------------- loaded spec:', code)
+  // let code = 'OpenAPI Example'
+
+  if(code) {
+    // eslint-disable-next-line
+    // @ts-ignore
+    window.updateSpec(code);
+    console.log('-------------- updateSpec with:', code)
+  }
+}
+
+
+window.onload = function () {
+  // Build a system
+  const ui = SwaggerUIBundle({
+    // url: "https://petstore.swagger.io/v2/swagger.json",
+    dom_id: "#swagger-ui",
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      // SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl,
+      SpecListener
+    ],
+    // requestSnippetsEnabled: true,
+    // layout: "StandaloneLayout"
+  })
+
+  // eslint-disable-next-line
+  // @ts-ignore
+  window.ui = ui
+
+  initializeMacro();
+}
