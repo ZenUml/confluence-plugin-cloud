78ebc474084fb8e9347ca8e3a6792e9881f4e4db
MrCoder <eagle.xiao@gmail.com>
2022-07-22 18:41:14 +1000

    Delay attachment creation for 1.5s.

--- src/sequence-viewer.ts
+++ src/sequence-viewer.ts
@@ -14,7 +14,7 @@ import {initializeMacro} from "@/model/macro/InitializeMacro";
 import createAttachmentIfContentChanged from "@/model/Attachment";
 import './GTagConfig'
 import globals from '@/model/globals';
-import {DiagramType} from "@/model/Diagram";
+import BaseMacro2 from "@/model/BaseMacro2";
 
 // eslint-disable-next-line
 // @ts-ignore
@@ -44,8 +44,25 @@ window.store = store
 EventBus.$on('diagramLoaded', () => {
   console.debug('Resize macro');
   // @ts-ignore
-  setTimeout(window.AP?.resize, 500)
+  setTimeout(window.AP?.resize, 1500)
 });
+
+async function createAttachment(macro: BaseMacro2) {
+  let diagramType = macro._diagram?.diagramType || 'unknown';
+  try {
+    if (await globals.apWrapper.canUserEdit()) {
+      trackEvent(diagramType, 'before_create_attachment', 'info');
+      await createAttachmentIfContentChanged(store.getters.content);
+    } else {
+      trackEvent(diagramType, 'skip_create_attachment', 'info');
+    }
+  } catch (e) {
+    // Do not re-throw the error
+    console.error('Error when creating attachment', e);
+    trackEvent(JSON.stringify(e), 'create_attachment' + diagramType, 'error');
+  }
+}
+
 EventBus.$on('diagramLoaded', async () => {
   // @ts-ignore
   const macro = globals.macro;
@@ -53,20 +70,9 @@ EventBus.$on('diagramLoaded', async () => {
 
     const canEdit = await macro.canEditOnDialog();
     store.dispatch('updateCanEdit', canEdit);
-
-    let diagramType = macro._diagram?.diagramType || 'unknown';
-    try {
-      if (await globals.apWrapper.canUserEdit()) {
-        trackEvent(diagramType, 'before_create_attachment', 'info');
-        await createAttachmentIfContentChanged(store.getters.content);
-      } else {
-        trackEvent(diagramType, 'skip_create_attachment', 'info');
-      }
-    } catch (e) {
-      // Do not re-throw the error
-      console.error('Error when creating attachment', e);
-      trackEvent(JSON.stringify(e), 'create_attachment' + diagramType, 'error');
-    }
+    setTimeout(async () => {
+      await createAttachment(macro);
+    }, 1500);
   }
 });
 
