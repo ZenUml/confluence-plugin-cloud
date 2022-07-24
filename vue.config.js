37f50c68a348ecf6e015ab16f949021e4831f147
MrCoder <eagle.xiao@gmail.com>
2022-07-24 10:07:24 +1000

    Fixed svg loading behaviour. (caused by upgrading vue-cli)

--- vue.config.js
+++ vue.config.js
@@ -94,17 +94,6 @@ module.exports = {
           },
         },
       });
-    // We need to clear the pre-built svg rule
-    // use `vue-cli-service inspect to check the webpack
-    // the built-in webpack uses file-loader
-    const svgRule = config.module.rule('svg')
-    svgRule.uses.clear();
-    svgRule
-      //.test(/\.svg$/)  // this is not required because the built-in svg rule already defined `test`
-      .use('svg-url-loader')
-      .loader('svg-url-loader')
-      .end()
-
   },
   productionSourceMap: false,
   configureWebpack: {
