df2edc106d43a2c6073b019a2540415a84922d3f
MrCoder <eagle.xiao@gmail.com>
2022-07-30 11:20:40 +1000

    Moved attachment and uninstalled API out of api folder.

--- functions/api/uninstalled.ts
+++
@@ -1,6 +0,0 @@
-export const onRequestGet: PagesFunction = async () => {
-  return new Response(
-    'OK',
-    {}
-  );
-};
--- functions/uninstalled.ts
+++ functions/uninstalled.ts
@@ -1,7 +1,7 @@
 import Toucan from 'toucan-js';
 
 export const onRequest: PagesFunction = async ({ request, waitUntil }) => {
-  console.log('onRequest: /installed');
+  console.log('onRequest: /uninstalled')
   const sentry = new Toucan({
     dsn: 'https://d7df1008a71541aca2063f58fe7fc0bf@o571476.ingest.sentry.io/6610196',
     context: {waitUntil, request}, // Includes 'waitUntil', which is essential for Sentry logs to be delivered. Also includes 'request' -- no need to set it separately.
@@ -14,9 +14,10 @@ export const onRequest: PagesFunction = async ({ request, waitUntil }) => {
     sentry.setTag('app-key', body.key);
     sentry.setTag('client-key', body.key);
     sentry.setTag('base-url', body.baseUrl);
-    sentry.captureMessage("{action: 'installed'}");
+    sentry.captureMessage("{action: 'uninstalled'}");
 
     return new Response(JSON.stringify('OK'),
+
       {
         status: 200,
         headers: {
@@ -30,4 +31,4 @@ export const onRequest: PagesFunction = async ({ request, waitUntil }) => {
       statusText: 'Internal Server Error',
     });
   }
-}
+};
