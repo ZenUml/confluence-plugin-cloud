9749cec0393fbd2e5c85b579794fc6320b94fb4e
MrCoder <eagle.xiao@gmail.com>
2022-07-30 13:03:47 +1000

    Moved error logging and handling to _middleware.ts.

--- functions/installed.ts
+++ functions/installed.ts
@@ -1,16 +1,9 @@
-import {captureError, captureInstalledMessage} from "./ConfigToucan";
+import {captureInstalledMessage} from "./ConfigToucan";
 import {OkResponse} from "./OkResponse";
-import {ServerErrorResponse} from "./ServerErrorResponse";
 
 export const onRequest: PagesFunction = async ({ request }) => {
   console.log('onRequest: /installed');
-  try {
-    const body = await request.json() as any;
-    captureInstalledMessage(body.key, body.clientKey, body.baseUrl);
-
-    return OkResponse();
-  } catch (err) {
-    captureError(err);
-    return ServerErrorResponse();
-  }
+  const body = await request.json() as any;
+  captureInstalledMessage(body.key, body.clientKey, body.baseUrl);
+  return OkResponse();
 }
--- functions/uninstalled.ts
+++ functions/uninstalled.ts
@@ -1,16 +1,9 @@
-import {captureError, captureUninstalledMessage} from "./ConfigToucan";
+import {captureUninstalledMessage} from "./ConfigToucan";
 import {OkResponse} from "./OkResponse";
-import {ServerErrorResponse} from "./ServerErrorResponse";
 
 export const onRequest: PagesFunction = async ({ request }) => {
   console.log('onRequest: /uninstalled')
-  try {
-    const body = await request.json() as any;
-    captureUninstalledMessage(body.key, body.clientKey, body.baseUrl);
-
-    return OkResponse();
-  } catch (err) {
-    captureError(err);
-    return ServerErrorResponse();
-  }
+  const body = await request.json() as any;
+  captureUninstalledMessage(body.key, body.clientKey, body.baseUrl);
+  return OkResponse();
 };
