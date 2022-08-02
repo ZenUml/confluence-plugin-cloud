97204cf42fb2151264cf4832f2a3b7c941ffc1f2
MrCoder <eagle.xiao@gmail.com>
2022-08-02 22:43:45 +1000

    Reimplemented va-radio-group with tailwindcss.
    The goal is to remove dependency on va-atlas completely which has stopped development.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="toolbar header">
-    <va-radio-group :vertical="false" v-model="diagramType">
-      <va-radio-btn label="sequence">ZenUML</va-radio-btn>
-      <va-radio-btn label="mermaid">Mermaid (Beta)</va-radio-btn>
-    </va-radio-group>
-
+    <div class="group p-0.5 rounded-lg flex bg-gray-100 hover:bg-gray-200">
+      <button type="button"
+              class="flex focus-visible:ring-2 focus-visible:ring-teal-500 focus-visible:ring-offset-2 rounded-md focus:outline-none focus-visible:ring-offset-gray-100"
+              :class="activeTab === 'sequence' ? 'bg-white shadow-sm ring-1 ring-black ring-opacity-5' : ''"
+              @click="setActiveTab('sequence')"
+              :tabindex="activeTab === 'sequence' ? '0' : '-1'">
+          <span class="p-1.5 lg:pl-2.5 lg:pr-3.5 rounded-md flex items-center text-sm font-medium"
+                :class="activeTab === 'sequence' ? 'bg-white shadow-sm ring-1 ring-black ring-opacity-5' : ''">
+            <span class="sr-only lg:not-sr-only text-gray-600 group-hover:text-gray-900"
+                  :class="activeTab === 'sequence' ? 'text-gray-900' : 'text-gray-600 group-hover:text-gray-900'">Sequence</span>
+          </span>
+      </button>
+      <button type="button"
+              class="ml-0.5 p-1.5 lg:pl-2.5 lg:pr-3.5 rounded-md flex items-center text-sm text-gray-600 font-medium focus-visible:ring-2 focus-visible:ring-teal-500 focus-visible:ring-offset-2 focus:outline-none focus-visible:ring-offset-gray-100"
+              :class="activeTab === 'mermaid' ? 'bg-white shadow-sm ring-1 ring-black ring-opacity-5' : ''"
+              @click="setActiveTab('mermaid')">
+      <span class="sr-only lg:not-sr-only text-gray-900"
+            :class="activeTab === 'mermaid' ? 'text-gray-900' : 'text-gray-600 group-hover:text-gray-900'">Mermaid</span>
+      </button>
+    </div>
     <a class="help mx-1" target="_blank" :href="helpUrl">
       <va-button type="default"><va-icon type="question-circle" /><span class="ml-1">Help</span></va-button>
     </a>
@@ -27,7 +42,17 @@ export default {
   },
   data() {
     return {
+      activeTab: 'sequence',
       helpUrl: 'https://zenuml.atlassian.net/wiki/spaces/Doc/overview',
+    }
+  },
+  mounted() {
+    // read diagram type from store and set active tab
+    const diagramType = this.$store.state.diagramType;
+    if (diagramType?.toLowerCase() === DiagramType.Mermaid?.toLowerCase()) {
+      this.setActiveTab('mermaid');
+    } else {
+      this.setActiveTab('sequence');
     }
   },
   computed: {
@@ -49,6 +74,17 @@ export default {
       }
     }
   },
+  methods: {
+    setActiveTab(tab) {
+      this.activeTab = tab;
+      let type = tab === 'sequence' ? DiagramType.Sequence : DiagramType.Mermaid;
+      console.log(type);
+      this.$store.dispatch('updateDiagramType', type)
+      if (type === DiagramType.Sequence) {
+        this.$store.dispatch('reloadZenUML')
+      }
+    },
+  }
 }
 </script>
 
