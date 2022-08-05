f9c96ffdf6ed20d8ba5543c0a55aa33dc377facb
MrCoder <eagle.xiao@gmail.com>
2022-08-05 14:12:11 +1000

    DocumentList does not require 'store'.

--- src/embed-editor.ts
+++ src/embed-editor.ts
@@ -1,33 +1,20 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-
 import DocumentList from './components/DocumentList.vue'
 
-// ==== CSS ====
-import './assets/tailwind.css'
-import 'vue-sequence/dist/vue-sequence.css'
-
-import ExtendedStore from './model/Store'
 import EventBus from './EventBus'
 import './GTagConfig'
 import globals from '@/model/globals';
 import AP from "@/model/AP";
+import './assets/tailwind.css'
 
 Vue.config.productionTip = false
 
-Vue.use(Vuex)
-
-const store = new Vuex.Store(ExtendedStore);
 if(document.getElementById('app')) {
     new Vue({
-      store,
       render: h => h(DocumentList)
     }).$mount('#app')
 }
 EventBus.$on('save', async () => {
-  // eslint-disable-next-line
-  // @ts-ignore
-  console.log('picked: ', window.picked);
 
   // eslint-disable-next-line
   // @ts-ignore
