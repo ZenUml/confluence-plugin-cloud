eed1aa8851220da650d92fca3101e4fee3210dda
MrCoder <eagle.xiao@gmail.com>
2022-08-05 12:26:05 +1000

    Export 'global' on `windows`.

--- src/model/globals/Global.ts
+++ src/model/globals/Global.ts
@@ -1,7 +1,10 @@
 import {IApWrapper} from "@/model/IApWrapper";
 import BaseMacro2 from "@/model/BaseMacro2";
 
-export default {
+let global = {
   macro: {} as BaseMacro2,
   apWrapper: {} as IApWrapper,
-}
+};
+// @ts-ignore
+window.global = global;
+export default global
