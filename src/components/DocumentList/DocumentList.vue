9b32517134b13f6448767b48e035f0d7a6fa46ad
MrCoder <eagle.xiao@gmail.com>
2022-08-06 10:44:59 +1000

    Added "recent diagrams and API specs" label.

--- src/components/DocumentList/DocumentList.vue
+++ src/components/DocumentList/DocumentList.vue
@@ -24,7 +24,7 @@
           <div class="flex flex-col w-full max-w-xs flex-grow border-l border-r">
             <div class="flex flex-shrink-0 items-center px-4 py-2 justify-between border-b">
               <button class="flex items-center text-xs font-semibold text-gray-600">
-                Sorted by Date
+                Recent diagrams and API specs
               </button>
             </div>
             <div class="flex-1 overflow-y-auto">
