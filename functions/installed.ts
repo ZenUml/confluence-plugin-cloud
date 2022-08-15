fdba2c4be9e703c0dbdac4c48758ccdac369a6fe
MrCoder <eagle.xiao@gmail.com>
2022-08-15 21:28:59 +1000

    Capture error but return OkResponse when request body is NOT json.

--- functions/installed.ts
+++ functions/installed.ts
@@ -1,9 +1,12 @@
-import {captureInstalledMessage} from "./ConfigToucan";
+import {captureError, captureInstalledMessage} from "./ConfigToucan";
 import {OkResponse} from "./OkResponse";
 
 export const onRequest: PagesFunction = async ({ request }) => {
-  console.log('onRequest: /installed');
-  const body = await request.json() as any;
-  captureInstalledMessage(body.key, body.clientKey, body.baseUrl);
+  try {
+    const body = await request.json() as any;
+    captureInstalledMessage(body.key, body.clientKey, body.baseUrl);
+  } catch (e: any) {
+    captureError(e)
+  }
   return OkResponse();
 }
--- functions/uninstalled.ts
+++ functions/uninstalled.ts
@@ -1,9 +1,12 @@
-import {captureUninstalledMessage} from "./ConfigToucan";
+import {captureError, captureUninstalledMessage} from "./ConfigToucan";
 import {OkResponse} from "./OkResponse";
 
 export const onRequest: PagesFunction = async ({ request }) => {
-  console.log('onRequest: /uninstalled')
-  const body = await request.json() as any;
-  captureUninstalledMessage(body.key, body.clientKey, body.baseUrl);
+  try {
+    const body = await request.json() as any;
+    captureUninstalledMessage(body.key, body.clientKey, body.baseUrl);
+  } catch (e: any) {
+    captureError(e)
+  }
   return OkResponse();
 };
