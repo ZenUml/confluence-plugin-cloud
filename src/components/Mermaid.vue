71b242165b1da4e957f6c85a42dd5cbae9fbccb3
MrCoder <eagle.xiao@gmail.com>
2022-08-28 20:37:23 +1000

    Added comment and empty code handling in Mermaid.vue

--- src/components/Mermaid.vue
+++ src/components/Mermaid.vue
@@ -20,16 +20,23 @@ export default {
   },
   computed: {
     code() {
-      // I don't know why we need `this.$store.state.diagramType === 'mermaid' &&` to trigger
-      // reactivity. Otherwise, mermaid diagram is not rendered by default.
+      // if we do not use `diagramType === 'mermaid' &&` this value will not change when we switch the tabs
+      // we cannot use mounted to initialise svg either (see comments in `mounted`).
       return this.$store.state.diagramType === 'mermaid' && this.$store.state.mermaidCode;
     }
   },
   async mounted() {
-    this.svg = await this.render(this.code);
+    // When it is firstly mounted, if diagramType !== 'mermaid', v-html will not inject the svg even if svg has value.
+    // This is because its parent has 'display:none'. So we cannot use mounted to inject svg node.
+  },
+  updated() {
+    // `updated` is also not triggered when diagramType changes, even after we mapped state `diagramType` to computed properties.
   },
   watch: {
+    // watch in general is not a good idea, but it seems that this is the only native way to trigger reactivity.
+    // another way would be use the https://www.npmjs.com/package/vue-async-computed
     async code() {
+      if (!this.code) return;
       this.svg = await this.render(this.code);
     }
   },
