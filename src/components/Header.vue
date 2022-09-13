40c1f1dca9b0c551baa03cf9f846e31b2708c006
MrCoder <eagle.xiao@gmail.com>
2022-09-13 13:07:21 +1000

    Tweaked button height.
    The Button was too high for the Graph Editor.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,7 +25,7 @@
         <send-feedback class="ml-2"/>
       </div>
       <a class="inline-block help mx-1 ml-2" target="_blank" :href="helpUrl">
-        <button class="flex items-center bg-gray-100 p-2 text-gray-600 text-sm font-semibold rounded-md" @click="saveAndExit">
+        <button class="flex items-center bg-gray-100 px-2 py-1 text-gray-600 text-sm font-semibold rounded-md" @click="saveAndExit">
           <span>
             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
           </span>
--- src/components/SendFeedback.vue
+++ src/components/SendFeedback.vue
@@ -1,12 +1,14 @@
 <template>
-<div class="send-feedback">
-  <a target="_blank" href="https://github.com/ZenUml/vue-sequence/discussions/352">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-    </svg>
-    Feedback on the new layout engine
-    <span class="inline-block w-2 h-2 mr-2 bg-red-600 rounded-full"></span>
-  </a>
+  <div class="send-feedback inline-block">
+    <a class="flex items-center px-2 py-1 text-sm rounded-md inline-block"
+       target="_blank" href="https://github.com/ZenUml/vue-sequence/discussions/352">
+      <span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+        </svg>
+      </span>
+      <span class="leading-none">Feedback on the new layout engine</span>
+    </a>
 </div>
 </template>
 
@@ -20,9 +22,6 @@
 
   .send-feedback {
     border-radius: 3px;
-    min-height: 34px;
-    line-height: 2.3em;
-    padding: 0 9px;
     background-color: #f3f4f6;
     color: #505e77;
   }
