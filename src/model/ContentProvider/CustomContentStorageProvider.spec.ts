83d2e51fb4626359cbdd73bfe8adb66b99552956
MrCoder <eagle.xiao@gmail.com>
2022-09-18 21:20:16 +1000

    Renamed test case.

--- src/model/ContentProvider/CustomContentStorageProvider.spec.ts
+++ src/model/ContentProvider/CustomContentStorageProvider.spec.ts
@@ -1,10 +1,9 @@
 import MockAp from '@/model/MockAp'
-import {ContentPropertyStorageProvider} from "@/model/ContentProvider/ContentPropertyStorageProvider";
 import {CustomContentStorageProvider} from "@/model/ContentProvider/CustomContentStorageProvider";
 import {NULL_DIAGRAM} from "@/model/Diagram/Diagram";
 import ApWrapper2 from "@/model/ApWrapper2";
 
-describe('ContentPropertyStorageProvider', () => {
+describe('CustomContentStorageProvider', () => {
   test('cannot find custom content', async () => {
     const mockAp = new MockAp(undefined);
     const storageProvider = new CustomContentStorageProvider(new ApWrapper2(mockAp));
