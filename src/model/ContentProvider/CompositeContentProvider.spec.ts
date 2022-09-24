3f45532819e81286fb00bb155265b508d6f7e5a0
MrCoder <eagle.xiao@gmail.com>
2022-09-24 22:35:43 +1000

    Added test for custom content native.

--- src/model/ContentProvider/CompositeContentProvider.spec.ts
+++ src/model/ContentProvider/CompositeContentProvider.spec.ts
@@ -1,7 +1,7 @@
 import MockAp from '@/model/MockAp'
-import {ContentPropertyStorageProvider} from "@/model/ContentProvider/ContentPropertyStorageProvider";
 import defaultContentProvider from "@/model/ContentProvider/CompositeContentProvider";
 import ApWrapper2 from "@/model/ApWrapper2";
+import TestHelper from '../../../tests/unit/TestHelper';
 
 describe('CompositeContentProvider', () => {
   test('should fallback to content property when no custom content id is provided', async () => {
@@ -27,6 +27,19 @@ describe('CompositeContentProvider', () => {
     const contentProvider = defaultContentProvider(new ApWrapper2(mockAp));
     const {doc} = (await contentProvider.load());
     expect(doc.code).toBe('body');
+  })
+
+  // This is used in embedding documents
+  test('should use url id if rendered.for==custom-content-native', async () => {
+    TestHelper.setUpUrlParam('rendered.for=custom-content-native&content.id=fake-content-id')
+    const mockAp = new MockAp('contentId');
+    // body will not be used
+    mockAp.confluence.saveMacro({}, 'body')
+
+    const contentProvider = defaultContentProvider(new ApWrapper2(mockAp));
+    const {doc} = (await contentProvider.load());
+    // `A.method` comes from MockAp.ts
+    expect(doc.code).toBe('A.method');
   })
 
 })
