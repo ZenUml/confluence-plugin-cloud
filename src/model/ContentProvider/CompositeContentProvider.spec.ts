d3564165dc4131cf19f26d93f9eee7b254cf6b04
MrCoder <eagle.xiao@gmail.com>
2022-09-24 17:42:47 +1000

    Added test case for falling back to content property if custom content id is not available.

--- src/model/ContentProvider/CompositeContentProvider.spec.ts
+++ src/model/ContentProvider/CompositeContentProvider.spec.ts
@@ -3,7 +3,19 @@ import {ContentPropertyStorageProvider} from "@/model/ContentProvider/ContentPro
 import defaultContentProvider from "@/model/ContentProvider/CompositeContentProvider";
 import ApWrapper2 from "@/model/ApWrapper2";
 
-describe('ContentPropertyStorageProvider', () => {
+describe('CompositeContentProvider', () => {
+  test('should fallback to content property when no custom content id is provided', async () => {
+    const mockAp = new MockAp('contentId');
+    mockAp.request = () => {
+      throw Error('custom content or content property not found')
+    }
+    mockAp.confluence.setContentProperty({key: 'key', version: {number: 1}, value: 'abcd'}, () => {})
+    mockAp.confluence.saveMacro({uuid: 'uuid'}, 'body')
+
+    const contentProvider = defaultContentProvider(new ApWrapper2(mockAp));
+    const {doc} = (await contentProvider.load());
+    expect(doc.code).toBe('abcd');
+  })
 
   test('should fallback to macro body when content property fails', async () => {
     const mockAp = new MockAp('contentId');
