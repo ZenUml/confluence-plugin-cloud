c796e3ee7e4fdd5065e758ef99bca9ba4d8c582c
MrCoder <eagle.xiao@gmail.com>
2022-09-26 21:31:58 +1000

    Added type declaration for descriptor.

--- functions/descriptor-dependencies/DescriptorUtil.spec.ts
+++ functions/descriptor-dependencies/DescriptorUtil.spec.ts
@@ -1,5 +1,7 @@
+import '../descriptor'
 import {getDescriptor, replaceUrls} from "./DescriptorUtil";
 import expectedDescriptor from "../test-data/expected-descriptor-full";
+import DynamicContentMacro = descriptor.DynamicContentMacro;
 const descriptor = require('../atlassian-connect.json');
 
 describe('DescriptorUtil', () => {
@@ -18,8 +20,9 @@ describe('DescriptorUtil', () => {
     expect(descriptor).toEqual(expectedDescriptor);
   });
 
-  it('should replace urls', function () {
-    descriptor.modules.dynamicContentMacros.forEach((macro: any) => {
+  it('makes sure all urls have version and addonKey', function () {
+    const modules = descriptor.modules;
+    modules.dynamicContentMacros.forEach((macro: any) => {
       // sequence-viewer.html?version=__VERSION__
       // &spaceKey={space.key}&pageId={page.id}&pageVersion={page.version}&macroId={macro.id}&uuid={uuid}&outputType={output.type}
       // &addonKey=__ADDON_KEY__&contentKey=__CONTENT_KEY__
@@ -28,13 +31,22 @@ describe('DescriptorUtil', () => {
       // TODO: is contentKey used anywhere?
       // expect(macro.editor.url).toContain('contentKey=__CONTENT_KEY__');
     });
-    replaceUrls(descriptor.modules, (url: string) => url);
-    const version = '2022.07';
-    const key = 'com.zenuml.confluence-addon';
-    expectedDescriptor.modules.dynamicContentMacros.forEach((macro: any) => {
-      expect(macro.editor.url).toContain(`version=${version}`);
-      expect(macro.editor.url).toContain(`addonKey=${key}`);
-      // expect(macro.editor.url).toContain(`contentKey=${getCustomContentKeyForModule(macro, expectedDescriptor.modules)}`);
+  })
+
+  it('should replace all urls under modules', function () {
+    const fakeUrl = 'fake-url';
+    const modules = descriptor.modules;
+    replaceUrls(modules, () => fakeUrl);
+    modules.dynamicContentMacros.forEach((macro: DynamicContentMacro) => {
+      expect(macro.editor.url).toBe(fakeUrl);
+      expect(macro.renderModes.default.url).toBe(fakeUrl);
     });
+    modules.generalPages.forEach((page: any) => {
+      expect(page.url).toBe(fakeUrl);
+    })
+    expect(modules.postInstallPage.url).toBe(fakeUrl);
+    modules.webPanels.forEach((panel: any) => {
+      expect(panel.url).toBe(fakeUrl);
+    })
   })
 })
---
+++ functions/descriptor.d.ts
@@ -0,0 +1,214 @@
+declare module descriptor {
+
+  export interface Authentication {
+    type: string;
+  }
+
+  export interface Lifecycle {
+    installed: string;
+    uninstalled: string;
+  }
+
+  export interface Links {
+    self: string;
+    homepage: string;
+  }
+
+  export interface Name {
+    value: string;
+  }
+
+  export interface Default {
+    url: string;
+  }
+
+  export interface RenderModes {
+    default: Default;
+  }
+
+  export interface Description {
+    value: string;
+  }
+
+  export interface EditTitle {
+    value: string;
+  }
+
+  export interface InsertTitle {
+    value: string;
+  }
+
+  export interface Editor {
+    url: string;
+    width: string;
+    height: string;
+    cacheable: boolean;
+    editTitle: EditTitle;
+    insertTitle: InsertTitle;
+  }
+
+  export interface Icon {
+    width: number;
+    height: number;
+    url: string;
+  }
+
+  export interface Documentation {
+    url: string;
+  }
+
+  export interface Name2 {
+    value: string;
+    i18n: string;
+  }
+
+  export interface Parameter {
+    identifier: string;
+    name: Name2;
+    type: string;
+    required: boolean;
+    multiple: boolean;
+    defaultValue: string;
+  }
+
+  export interface DynamicContentMacro {
+    key: string;
+    name: Name;
+    renderModes: RenderModes;
+    url: string;
+    description: Description;
+    outputType: string;
+    bodyType: string;
+    editor: Editor;
+    icon: Icon;
+    documentation: Documentation;
+    categories: string[];
+    featured: boolean;
+    parameters: Parameter[];
+  }
+
+  export interface ContentViewComponent {
+    moduleKey: string;
+  }
+
+  export interface Item {
+    width: number;
+    height: number;
+    url: string;
+  }
+
+  export interface Icons {
+    item: Item;
+  }
+
+  export interface UiSupport {
+    contentViewComponent: ContentViewComponent;
+    icons: Icons;
+  }
+
+  export interface Indexing {
+    enabled: boolean;
+  }
+
+  export interface ApiSupport {
+    bodyType: string;
+    supportedContainerTypes: string[];
+    supportedChildTypes: string[];
+    indexing: Indexing;
+  }
+
+  export interface Name3 {
+    value: string;
+  }
+
+  export interface CustomContent {
+    uiSupport: UiSupport;
+    apiSupport: ApiSupport;
+    name: Name3;
+    key: string;
+  }
+
+  export interface Name4 {
+    value: string;
+  }
+
+  export interface GeneralPage {
+    key: string;
+    location: string;
+    url: string;
+    name: Name4;
+  }
+
+  export interface Name5 {
+    value: string;
+  }
+
+  export interface PostInstallPage {
+    key: string;
+    name: Name5;
+    url: string;
+  }
+
+  export interface Layout {
+    width: string;
+    height: string;
+  }
+
+  export interface Name6 {
+    value: string;
+  }
+
+  export interface Params {
+    permission: string;
+  }
+
+  export interface Condition {
+    condition: string;
+    params: Params;
+  }
+
+  export interface WebPanel {
+    url: string;
+    location: string;
+    layout: Layout;
+    weight: number;
+    supportsNative: boolean;
+    name: Name6;
+    key: string;
+    conditions: Condition[];
+  }
+
+  export interface Modules {
+    dynamicContentMacros: DynamicContentMacro[];
+    customContent: CustomContent[];
+    generalPages: GeneralPage[];
+    postInstallPage: PostInstallPage;
+    webPanels: WebPanel[];
+  }
+
+  export interface ApiMigrations {
+    gdpr: boolean;
+  }
+
+  export interface Vendor {
+    name: string;
+    url: string;
+  }
+
+  export interface RootObject {
+    key: string;
+    name: string;
+    description: string;
+    baseUrl: string;
+    authentication: Authentication;
+    lifecycle: Lifecycle;
+    enableLicensing: boolean;
+    links: Links;
+    scopes: string[];
+    modules: Modules;
+    apiMigrations: ApiMigrations;
+    vendor: Vendor;
+  }
+
+}
+
