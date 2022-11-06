737049ba3f168a4d537e944f801b0aac84c38d70
Yanhui Li <whimet@gmail.com>
2022-11-06 18:57:21 +1100

    fix test

--- src/model/ContentProvider/Persistence.spec.ts
+++ src/model/ContentProvider/Persistence.spec.ts
@@ -3,6 +3,7 @@ import {NULL_DIAGRAM} from "@/model/Diagram/Diagram";
 
 const mockSave = jest.fn(() => "mocked_custom_content_id");
 const mockSaveMacro = jest.fn();
+const mockIsInContentEdit = jest.fn();
 const mockGetMacroData = () => {
   return {
     "uuid": "uuid_from_macro_data"
@@ -18,9 +19,10 @@ jest.mock("@/model/ContentProvider/CustomContentStorageProvider", () => {
 })
 
 jest.mock("@/model/ApWrapper2", () => {
-  return class ApWrapper2{
+  return class ApWrapper2 {
     getMacroData = mockGetMacroData
     saveMacro = mockSaveMacro
+    isInContentEdit = mockIsInContentEdit
   }
 })
 
@@ -29,9 +31,24 @@ jest.mock('@/utils/uuid', () => {
 })
 
 describe('Persistence', function () {
-  it('should save the diagram', async () => {
+
+  beforeEach(() => {
+    mockSaveMacro.mockClear();
+    mockIsInContentEdit.mockClear();
+    mockSave.mockClear();
+  });
+
+  it('should save the diagram in content edit mode', async () => {
+    mockIsInContentEdit.mockReturnValue(true);
     await saveToPlatform(NULL_DIAGRAM);
     expect(mockSave).toBeCalledWith(NULL_DIAGRAM);
     expect(mockSaveMacro).toBeCalledWith(expect.objectContaining({"uuid": "uuid_from_macro_data", "customContentId": "mocked_custom_content_id"}), '')
+  })
+
+  it('should not save macro in content view mode', async () => {
+    mockIsInContentEdit.mockReturnValue(false);
+    await saveToPlatform(NULL_DIAGRAM);
+    expect(mockSave).toBeCalledWith(NULL_DIAGRAM);
+    expect(mockSaveMacro.mock.calls.length).toBe(0);
   })
 });
