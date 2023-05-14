04a24c75a6c3255b6f55a534fae53e9e04f15ddf
Yanhui Li <whimet@gmail.com>
2023-05-14 21:29:46 +1000

    Update cloudflare to proxy installed request to ACE server

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,7 +1,30 @@
 import {captureError, ConfigToucan} from "./ConfigToucan";
 import {ServerErrorResponse} from "./ServerErrorResponse";
 
-export const onRequest: PagesFunction = async ({next, request, waitUntil}) => {
+const proxyToACE: PagesFunction = async ({next, request, waitUntil, env}) => {
+  console.log('-- in reverseProxy');
+  const aceBaseUrl = env.ACE_BASEURL;
+
+  if(request.url.includes('/installed') && aceBaseUrl) {
+    console.log('ACE_BASEURL: ', aceBaseUrl);
+    try {
+      const body = await request.json();
+      const headers = request.headers;
+
+      console.log(body);
+      // console.log(Array.from(request.headers.entries()));
+
+      const newUrl = `${aceBaseUrl}/installed${new URL(request.url).search}`;
+
+      await fetch(newUrl, {method: 'POST', body: JSON.stringify(body), headers});
+    } catch(e) {
+      console.error(e);
+    }
+  }
+  return await next();
+}
+
+const configToucan: PagesFunction = async ({next, request, waitUntil}) => {
   try {
     ConfigToucan(request, waitUntil);
     return await next();
@@ -10,3 +33,5 @@ export const onRequest: PagesFunction = async ({next, request, waitUntil}) => {
     return ServerErrorResponse();
   }
 }
+
+export const onRequest: PagesFunction[] = [proxyToACE, configToucan]
