5a4e1ce65b18408840336db7c39bb2c764b955dd
MrCoder <eagle.xiao@gmail.com>
2023-08-16 22:05:18 +1000

    Added generic-viewer to portal.

--- src/components/DiagramPortal.vue
+++ src/components/DiagramPortal.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
+  <generic-viewer>
     <div v-if="diagramType==='mermaid'">mermaid viewer</div>
     <div v-if="diagramType==='sequence'">sequence viewer</div>
-  </div>
+  </generic-viewer>
 </template>
 <script>
+import GenericViewer from "@/components/Viewer/GenericViewer.vue";
+
 export default {
   name: "DiagramPortal",
+  components: {GenericViewer},
   computed: {
     diagramType() {
       return this.$store.state.diagram.diagramType;
