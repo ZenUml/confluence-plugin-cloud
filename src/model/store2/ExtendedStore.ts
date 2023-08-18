63b513d44ef1474d4da42f8e1357fe3c1f4f9261
MrCoder <eagle.xiao@gmail.com>
2023-08-18 20:25:35 +1000

    Removed store from @zenuml/core.

--- src/model/store2/ExtendedStore.ts
+++ src/model/store2/ExtendedStore.ts
@@ -1,16 +1,12 @@
-import {VueSequence} from '@zenuml/core';
 import { StoreOptions } from 'vuex';
 import { RootState} from "@/model/store2/types";
 import {DiagramType, NULL_DIAGRAM} from "@/model/Diagram/Diagram";
 import globals from "@/model/globals";
 import Example from "@/utils/sequence/Example";
 import EventBus from "@/EventBus";
-const storeConfig = VueSequence.Store()
 
 const ExtendedStore: StoreOptions<RootState> = {
-  ...storeConfig,
   mutations: {
-    ...storeConfig.mutations,
     updateCode2(state: any, payload: any) {
       state.code = payload
       state.diagram.code = payload
@@ -25,7 +21,6 @@ const ExtendedStore: StoreOptions<RootState> = {
     },
   },
   actions: {
-    ...storeConfig.actions,
     updateCode2({commit}: any, payload: any) {
       commit('updateCode2', payload)
     },
@@ -42,14 +37,13 @@ const ExtendedStore: StoreOptions<RootState> = {
     }
   },
   getters: {
-    ...storeConfig.getters,
     svg: (state: any) => {
       return state.mermaidSvg
     },
     isDisplayMode: () => globals.apWrapper.isDisplayMode(),
   },
   state: {
-    ...storeConfig.state,
+    code: Example.Sequence,
     mermaidCode: Example.Mermaid,
     diagramType: DiagramType.Sequence,
     mermaidSvg: '',
--- src/model/store2/types.ts
+++ src/model/store2/types.ts
@@ -3,6 +3,9 @@ import {Diagram, DiagramType} from "@/model/Diagram/Diagram";
 export interface RootState {
   diagram: Diagram
   mermaidCode: string | undefined,
+  mermaidSvg: string | undefined,
   code: string | undefined,
-  diagramType: DiagramType
+  diagramType: DiagramType,
+  error: any,
+  onElementClick: Function
 }
