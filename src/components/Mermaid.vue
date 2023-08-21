d4b2497e25f599eb56bce753ac7305e48ad406f2
MrCoder <eagle.xiao@gmail.com>
2023-08-22 09:33:39 +1000

    Updated comments.

--- src/components/Mermaid.vue
+++ src/components/Mermaid.vue
@@ -21,24 +21,18 @@ export default {
   },
   computed: {
     code() {
-      // if we do not use `diagramType === 'mermaid' &&` this value will not change when we switch the tabs
-      // we cannot use mounted to initialise svg either (see comments in `mounted`).
       return this.$store.state.diagram.diagramType === 'mermaid' && this.$store.state.diagram.mermaidCode;
     }
   },
   async mounted() {
-    // When it is firstly mounted, if diagramType !== 'mermaid', v-html will not inject the svg even if svg has value.
-    // This is because its parent has 'display:none'. So we cannot use mounted to inject svg node.
     if (!this.code) return;
     this.svg = await this.render(this.code);
     EventBus.$emit('diagramLoaded', this.$store.state.diagram.code, this.$store.state.diagram.diagramType);
   },
   updated() {
-    // `updated` is also not triggered when diagramType changes, even after we mapped state `diagramType` to computed properties.
+    // Don't use updated() to render, because it will cause infinite loop.
   },
   watch: {
-    // watch in general is not a good idea, but it seems that this is the only native way to trigger reactivity.
-    // another way would be use the https://www.npmjs.com/package/vue-async-computed
     async code() {
       if (!this.code) return;
       this.svg = await this.render(this.code);
