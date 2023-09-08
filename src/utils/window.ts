7070ad2b6561ec4eed6bec0c93b3d36a6e8d26c0
Yanhui Li <whimet@gmail.com>
2023-09-08 21:36:26 +1000

    Apply R2 track in trackEvent

--- src/utils/window.ts
+++ src/utils/window.ts
@@ -1,5 +1,5 @@
 import {DiagramType} from "@/model/Diagram/Diagram";
-import {getClientDomain, getSpaceKey} from "@/utils/ContextParameters/ContextParameters";
+import {getBaseUrl, getClientDomain, getSpaceKey} from "@/utils/ContextParameters/ContextParameters";
 export function getUrlParam (param: string): string | undefined {
   try {
     const matches = (new RegExp(param + '=([^&]*)')).exec(window.location.search);
@@ -34,16 +34,41 @@ export function trackEvent(label: DiagramType | string, action: string, category
     } catch (e) {
       console.error(e);
     }
+    
+    try {
+      // @ts-ignore
+      window.gtag && window.gtag('event', action, eventDetails);
+    } catch(e) {
+      console.log('Error in calling gtag', e);
+    }
+
     // @ts-ignore
-    window.gtag && window.gtag('event', action, eventDetails);
-    // @ts-ignore
-    zaraz?.track(action, eventDetails);
+    try {
+      // @ts-ignore
+      zaraz && zaraz.track(action, eventDetails);
+    } catch(e) {
+      console.log('Error in calling zaraz', e);
+    }
+
+    r2Track(action, eventDetails);
   } catch (e) {
-    console.error('Error in trackingEvent. Please report to our helpdesk: https://zenuml.atlassian.net/servicedesk/customer/portals')
+    console.error('Error in trackingEvent. Please report to our helpdesk: https://zenuml.atlassian.net/servicedesk/customer/portals', e)
   }
 }
 
 function getCurrentUserAccountId(): string {
   // @ts-ignore
   return window.globals?.apWrapper?.currentUser?.atlassianAccountId || 'unknown_user_account_id';
+}
+
+function r2Track(action: string, eventDetails: any) {
+  try {
+    fetch('/track', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(Object.assign({baseUrl: getBaseUrl(), action}, eventDetails))
+    });
+  } catch(e) {
+    console.log('Error in calling /track', e)
+  }
 }
