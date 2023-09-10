9411c751522933694a866748befe3e3302866d91
MrCoder <eagle.xiao@gmail.com>
2023-09-10 13:43:10 +1000

    Improved sticky participants further by configuring stickyOffset due to host page header.

--- src/components/Sequence.vue
+++ src/components/Sequence.vue
@@ -20,15 +20,23 @@ export default {
   async mounted() {
     console.debug('SequenceCanvas - mounted', );
     zenuml = new ZenUml(this.$refs['zenuml']);
-    await zenuml.render(this.$store.state.diagram.code, 'theme-default');
+    await this.render();
     EventBus.$emit('diagramLoaded', this.$store.state.diagram.code, this.$store.state.diagram.diagramType);
+  },
+  methods: {
+    async render() {
+      // stickyOffset is used only at view mode or edit when the iframe scroll out of the viewport
+      // In fullscreen viewer or editor mode, the iFrame element is not scrollable, so we don't need to offset.
+      // Note when the iframe is not scrollable, the stickyOffset does not have any effect.
+      await zenuml.render(this.$store.state.diagram.code, {theme:'theme-default', stickyOffset: 56});
+    }
   },
   watch: {
     // watch in general is not a good idea, but it seems that this is the only native way to trigger reactivity.
     // another way would be use the https://www.npmjs.com/package/vue-async-computed
     async code() {
       if (!this.code) return;
-      await zenuml.render(this.$store.state.diagram.code, 'theme-default');
+      await this.render();
     }
   },
 
