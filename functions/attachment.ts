1699c75fc96eb2aa4076a8e5236f95370a00bd59
dontry <mccoy018@gmail.com>
2023-12-05 23:29:46 +1100

    Update image width in attachment.ts

--- functions/attachment.ts
+++ functions/attachment.ts
@@ -2,7 +2,7 @@ export const onRequestGet: PagesFunction = async (params: any) => {
   const { searchParams } = new URL(params.request.url);
   const uuid = searchParams.get('uuid');
 	return new Response(
-		`<ac:image> <ri:attachment ri:filename="zenuml-${uuid}.png" /> </ac:image>`,
+		`<ac:image ac:width="950"> <ri:attachment ri:filename="zenuml-${uuid}.png" /> </ac:image>`,
 		{}
 	);
 };
