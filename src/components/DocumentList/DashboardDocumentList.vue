c62a982651a791ed6a283de0238ea33b8bdf6eef
Yanhui Li <whimet@gmail.com>
2023-12-07 20:24:51 +1100

    Page link works in dashboard

--- src/components/DocumentList/DashboardDocumentList.vue
+++ src/components/DocumentList/DashboardDocumentList.vue
@@ -34,7 +34,8 @@
               <div v-for="containerPage in filteredPageList" :key="containerPage.id" class="block px-6 py-3 bg-white border-t hover:bg-gray-50">
                 <div class="mt-2 text-sm text-gray-600">
                   <!-- :href="`${baseUrl}${ containerPage.id }`" -->
-                  <a target="_blank" class="flex items-center justify-between hover:underline group">
+                  <a href="#" class="flex items-center justify-between hover:underline group" @click="gotoPage(containerPage.id)">
+
                     <span class="inline-block truncate">Page: {{ containerPage.title }}</span>
                     <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-5 w-5 flex-shrink-0 invisible group-hover:visible" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
@@ -188,6 +189,9 @@
     methods: {
       setFilter(docType) {
         this.docTypeFilter = docType;
+      },
+      gotoPage(pageId) {
+        AP.navigator.go('contentview', {contentId: pageId});
       }
     },
     components: {
