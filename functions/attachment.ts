360e12d85e772e981d44ff6a1f0aa812653ba17f
unimu-cic <kkcarson1874@gmail.com>
2023-12-19 17:51:27 +0800

    feat: add attachment download track

--- functions/attachment.ts
+++ functions/attachment.ts
@@ -1,8 +1,15 @@
+import {trackEvent} from "../src/utils/window";
+
 export const onRequestGet: PagesFunction = async (params: any) => {
-  const { searchParams } = new URL(params.request.url);
+  const {searchParams} = new URL(params.request.url);
+
   const uuid = searchParams.get('uuid');
-	return new Response(
-		`<ac:image ac:width="950"> <ri:attachment ri:filename="zenuml-${uuid}.png" /> </ac:image>`,
-		{}
-	);
+  const domain = searchParams.get('xdm_e');
+
+  trackEvent(domain || 'unknown_domain', 'download_attachment', 'export');
+
+  return new Response(
+    `<ac:image ac:width="950"> <ri:attachment ri:filename="zenuml-${uuid}.png" /> </ac:image>`,
+    {}
+  );
 };
