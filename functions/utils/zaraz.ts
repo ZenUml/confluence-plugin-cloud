1dda89513b500ff8bb8d148ad47acf47980dd06f
unimu_ <125171539+unimu-cic@users.noreply.github.com>
2023-12-21 14:02:05 +0800

    feat: add attachment download track (#573)
    * feat: add attachment download track
    
    * fix: use  instead of resolving external function
    
    * chore: add try catch block

--- functions/utils/zaraz.ts
+++ functions/utils/zaraz.ts
@@ -2,6 +2,48 @@ import fetch from 'node-fetch';
 
 const HTTP_API_ENDPOINT = 'https://zenuml.com/zaraz/api';
 
+
+interface EventParams {
+  eventType: string
+  [key: string]: any
+}
+
+async function trackEvent({eventType, ...restProps}: EventParams) {
+  const additionalProps = Object.keys(restProps)
+    .map((key: string) => `"${key}": "${restProps[key]}"`)
+    .join(', ');
+
+  const body = `{
+    "events": [
+      {
+        "client": {
+            "__zarazTrack": "${eventType}",
+            ${additionalProps}
+        }
+      }
+    ]
+  }`
+
+  const request = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: body,
+  };
+
+  try {
+    const response = await fetch(HTTP_API_ENDPOINT, request);
+
+    const data = await response.text(); // assuming server responds with json
+    console.log(data);
+  } catch (error) {
+    console.error('Error sending event:', error);
+  }
+}
+
+
+// TODO: use trackEvent instead of
 async function postData(eventType: string, appKey: string, clientKey: string, clientSite: string) {
   const body = `{
             "events": [
@@ -29,4 +71,7 @@ async function postData(eventType: string, appKey: string, clientKey: string, cl
   console.log(data);
 }
 
-export {postData}
+export {
+  postData,
+  trackEvent
+}
