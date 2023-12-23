f2762b700f9a164e03e5ed76d1f5556036d63199
unimu-cic <kkcarson1874@gmail.com>
2023-12-23 18:39:27 +0800

    feat: update styles

--- src/components/CSAT/index.vue
+++ src/components/CSAT/index.vue
@@ -2,24 +2,24 @@
   <transition name="fade">
     <div
       v-if="open"
-      class="fixed w-[470px] h-fit bottom-4 right-4 z-50 bg-white text-[#172B4D] p-10 rounded-lg drop-shadow-[0_20px_26px_rgba(176,176,176,0.35)]"
+      class="fixed w-[470px] h-fit bottom-4 right-4 z-50 bg-white text-[#282828] py-10 px-12 rounded-xl drop-shadow-[0_20px_26px_rgba(176,176,176,0.35)]"
     >
       <div v-if="!hasFeedback">
-        <p class="font-medium text-lg mt-4">
+        <p class="font-bold text-lg">
           Rate your experience with ZenUML
         </p>
-        <div class="mt-3 flex justify-between text-sm">
+        <div class="mt-8 flex justify-between text-sm">
           <div
             v-for="(value, index) in score"
             :key="value"
             @click="handleSetValue(index + 1)"
-            class="bg-[#F1F1F1] w-12 h-12 rounded-lg border-2 p-1.5 text-2xl flex items-center justify-center group hover:bg-[#e0e0e0] duration-200 cursor-pointer"
+            class="bg-[#F1F1F1] w-12 h-12 rounded-lg border-2 p-1.5 text-3xl flex items-center justify-center group hover:bg-[#e0e0e0] duration-200 cursor-pointer"
             :class="csatVal === index + 1 ? 'border-[#444BFF]' : 'border-transparent'"
           >
             {{ value }}
           </div>
         </div>
-        <div class="mt-2.5 text-xs text-[#939393] flex justify-between">
+        <div class="mt-2.5 text-sm text-[#939393] flex justify-between">
           <span>Very bad</span>
           <span>Very good</span>
         </div>
@@ -40,7 +40,7 @@
               target="_blank"
               @click="handleTellUsMoreClick"
               data-event-label="csat_tell_us_more"
-              class="px-7 flex justify-center h-[52px] items-center flex-1 py-3 bg-[#282828] rounded-lg text-white"
+              class="px-7 text-base flex justify-center h-[52px] items-center flex-1 py-3 bg-[#282828] rounded-lg text-white"
               href="https://github.com/orgs/ZenUml/discussions"
             >Tell us more</a>
           </div>
@@ -49,7 +49,7 @@
       <div>
         <transition name="fade-no-transform">
           <div
-            class="bg-inherit rounded-lg flex flex-col"
+            class="bg-inherit rounded-lg flex flex-col py-[18px] -mx-[10px]"
             v-if="hasFeedback && csatVal"
           >
             <div class="flex gap-2">
@@ -58,7 +58,7 @@
                 <path d="M12.7119 18L16.4619 21.75L23.9619 14.25" stroke="white" stroke-width="1.125"
                       stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
-              <span class="text-center text-3xl">Thank You</span>
+              <span class="text-center text-4xl font-bold">Thank You</span>
             </div>
             <p class="mt-4">Your review was successfully submitted.</p>
             <div class="flex justify-end mt-8">
@@ -91,7 +91,7 @@ onMounted(async () => {
   if (!isPopped) {
     timer = setTimeout(() => {
       open.value = true;
-    }, 1000 * 60)
+    }, 1000 * 2)
   }
 });
 
