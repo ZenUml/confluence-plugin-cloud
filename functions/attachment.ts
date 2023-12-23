da4962fbbc81486cc3eb78e3f5ada3d20936727f
unimu-cic <kkcarson1874@gmail.com>
2023-12-23 11:22:54 +0800

    feat: enhance attachment event tracking

--- functions/attachment.ts
+++ functions/attachment.ts
@@ -3,13 +3,26 @@ import {trackEvent} from "./utils/zaraz";
 export const onRequestGet: PagesFunction = async (params: any) => {
   const {searchParams} = new URL(params.request.url);
 
-  const uuid = searchParams.get('uuid');
-  const domain = searchParams.get('xdm_e');
+  const uuid = searchParams.get('uuid') || '';
+  const baseURL = searchParams.get('xdm_e') || '';
+  const spaceName = searchParams.get('spaceKey') || '';
+  const pageId = searchParams.get('pageId') || '';
+  const appKey = searchParams.get('addonKey') || '';
 
-  await trackEvent({
-    eventType: 'download_attachment',
-    domain
-  });
+  const domain = baseURL ? new URL(baseURL).hostname : 'unknown_domain';
+
+  try {
+    await trackEvent({
+      eventType: 'retrieve_attachment',
+      event_label: uuid,
+      confluence_page: pageId,
+      confluence_space: spaceName,
+      app_key: appKey,
+      domain
+    });
+  } catch (error) {
+    console.error("Failed to track retrieve_attachment: ", error);
+  }
 
   return new Response(
     `<ac:image ac:width="950"> <ri:attachment ri:filename="zenuml-${uuid}.png" /> </ac:image>`,
