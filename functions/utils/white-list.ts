ffda84821be1bcded0dd0f1006e369234c5ef198
danshuitaihejie <474182370@qq.com>
2024-01-13 00:25:08 +0800

    feat: using * to match all domain

--- functions/utils/white-list.ts
+++ functions/utils/white-list.ts
@@ -1,14 +1,11 @@
-//The following two environment variables need to be set on https://dash.cloudflare.com/xxxxxxx/pages/view/confluence-pulgin/settings/environment-variables :
-//1: WHITE_LIST_ENABLED:true  #When not enabled, it will affect all subdomains.
-//2: WHITE_LIST_NAMES:	subDomain1,subDomain2
+//The following  environment variables need to be set on https://dash.cloudflare.com/xxxxxxx/pages/view/confluence-pulgin/settings/environment-variables :
+//WHITE_LIST:	subDomain1,subDomain2 #if contains * ,will effect all domain.
 function checkWhiteList(env: any, subDomain: string) {
-  console.log("checkWhiteList env",subDomain,env.WHITE_LIST_ENABLED,env.WHITE_LIST_NAMES)
-  const whiteListEnabled: boolean = env.WHITE_LIST_ENABLED === 'true';
-  if (!whiteListEnabled) return true;
-  const whiteListNames: string[] = env.WHITE_LIST_NAMES ? env.WHITE_LIST_NAMES.split(",").map(name => name.trim().toLowerCase()) : [];
-  const result= whiteListNames.includes(subDomain.toLowerCase());
-  console.log("checkWhiteList result",result,whiteListNames);
-  return result;
+  const whiteListNames: string[] = env.WHITE_LIST
+    ? env.WHITE_LIST.split(",").map(name => name.trim().toLowerCase()).filter(name => name !== "")
+    : [];
+  if(whiteListNames.includes("*"))return true;
+  return whiteListNames.includes(subDomain.toLowerCase());
 }
 
 export {
