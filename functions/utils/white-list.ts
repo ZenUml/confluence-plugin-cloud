62197289e9acf034c4c7668263270672d921202a
danshuitaihejie <474182370@qq.com>
2024-01-20 16:08:38 +0800

    fix: pdf exported normally without kv when checking white list

--- functions/utils/white-list.ts
+++ functions/utils/white-list.ts
@@ -1,9 +1,14 @@
 async function checkWhiteList(kv: any, key: string, subDomain: string) {
-  const names = await kv.get(key);
-  //whiteListNames:	subDomain1,subDomain2 #if contains * ,will effect all domain.
-  const whiteListNames: string[] =  names? names.split(",").map(name => name.trim().toLowerCase()).filter(name => name !== ""): [];
-  if(whiteListNames.includes("*"))return true;
-  return whiteListNames.includes(subDomain.toLowerCase());
+  try {
+    const names = await kv.get(key);
+    //whiteListNames:	subDomain1,subDomain2 #if contains * ,will effect all domain.
+    const whiteListNames: string[] =  names? names.split(",").map(name => name.trim().toLowerCase()).filter(name => name !== ""): [];
+    if(whiteListNames.includes("*"))return true;
+    return whiteListNames.includes(subDomain.toLowerCase());
+  } catch (error) {
+    console.error("Failed to check white list: ", error);
+  }
+  return false;
 }
 
 export {
